210708c5e64bf2202d924e03ecafad0db3aacf0f
James Addyman <addyman@me.com>
2013-08-07 20:52:04 +0100

    Initial Commit

---
+++ Provenance/PVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PVAppDelegate.h
+//  Provenance
+//
+//  Created by James Addyman on 07/08/2013.
+//  Copyright (c) 2013 James Addyman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
