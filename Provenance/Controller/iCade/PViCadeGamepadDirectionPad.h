68fc0d57a0554d2f405c88756dc37006f048f66c
Josejulio Martínez Magaña <josejulio.martinez@aluxoft.com>
2015-06-22 11:40:11 -0500

    Changed name.

--- Provenance/Controller/iCade/PViCadeGamepadDirectionPad.h
+++ Provenance/Controller/iCade/PViCadeGamepadDirectionPad.h
@@ -3,7 +3,7 @@
 //  Provenance
 //
 //  Created by Josejulio Martínez on 19/06/15.
-//  Copyright (c) 2015 James Addyman. All rights reserved.
+//  Copyright (c) 2015 Josejulio Martínez. All rights reserved.
 //
 
 #import <GameController/GameController.h>
