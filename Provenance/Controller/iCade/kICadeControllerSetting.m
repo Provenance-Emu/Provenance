a5143516713820cab9a9e0d33bdcc79e164fa42c
Josejulio Martínez Magaña <josejulio.martinez@aluxoft.com>
2015-07-10 23:40:32 -0500

    iCade enum to identify the controller with options to String and PVICadeController

---
+++ Provenance/Controller/iCade/kICadeControllerSetting.m
@@ -0,0 +1,46 @@
+//
+//  kICadeControllerSetting.c
+//  Provenance
+//
+//  Created by Josejulio Martínez on 10/07/15.
+//  Copyright (c) 2015 Josejulio Martínez. All rights reserved.
+//
+
+#include "kICadeControllerSetting.h"
+#import "PViCade8BitdoController.h"
+
+
+NSString* kIcadeControllerSettingToString(kICadeControllerSetting value) {
+    NSString* stringRepresentation = nil;
+    switch (value) {
+        case kICadeControllerSettingDisabled:
+            stringRepresentation = @"Disabled";
+            break;
+        case kICadeControllerSettingStandard:
+            stringRepresentation = @"Standard Controller";
+            break;
+        case kICadeControllerSetting8Bitdo:
+            stringRepresentation = @"8bitdo Nes30 Controller";
+        default:
+            break;
+    }
+    
+    return stringRepresentation;
+}
+
+PViCadeController* kIcadeControllerSettingToPViCadeController(kICadeControllerSetting value) {
+    PViCadeController* controller = nil;
+    switch (value) {
+        case kICadeControllerSettingDisabled:
+            controller = nil;
+            break;
+        case kICadeControllerSettingStandard:
+            controller = [[PViCadeController alloc] init];
+            break;
+        case kICadeControllerSetting8Bitdo:
+            controller = [[PViCade8BitdoController alloc] init];
+        default:
+            break;
+    }
+    return controller;
+}
