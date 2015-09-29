27d2e7848a8ffa694f4fec69971a78fe60dd7345
James Addyman <addyman@me.com>
2015-09-29 22:14:50 +0100

    3D touch for recently played games

--- Provenance/PVAppDelegate.h
+++ Provenance/PVAppDelegate.h
@@ -10,6 +10,7 @@
 
 @interface PVAppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong) UIApplicationShortcutItem *shortcutItem;
 
 @end
