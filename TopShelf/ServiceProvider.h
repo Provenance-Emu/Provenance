5cdda2191369e145fff3e4f047a04614722d27e3
David Muzi <david.muzi@shopify.com>
2015-12-16 21:44:10 -0500

    instructions correction

--- TopShelf/ServiceProvider.h
+++ TopShelf/ServiceProvider.h
@@ -14,7 +14,7 @@
  1. Enable App Groups on the TopShelf target, and specify an App Group ID
         Provenance Project -> TopShelf Target -> Capabilities Section -> App Groups
  2. Enable App Groups on the Provenance TV target, using the same App Group ID
- 3. Define the value for `PVAppGroupId` in `RLMRealmConfiguration+GroupConfig.m` to that App Group ID
+ 3. Define the value for `PVAppGroupId` in `PVAppConstants.m` to that App Group ID
  
  */
 
