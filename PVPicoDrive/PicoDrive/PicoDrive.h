3570625c6c0690333e06a7d2d4f1c66b803ba160
Joe Mattiello <git@joemattiello.com>
2016-11-25 07:10:51 -0500

    PicoDrive git ignore was blocking required directory

---
+++ PVPicoDrive/PicoDrive/PicoDrive.h
@@ -0,0 +1,19 @@
+//
+//  PicoDrive
+//  Stella
+//
+//  Created by Joseph Mattiello on 2/28/16.
+//  Copyright © 2016 JamSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PicoDrive.
+FOUNDATION_EXPORT double PicoDriveVersionNumber;
+
+//! Project version string for PicoDrive.
+FOUNDATION_EXPORT const unsigned char PicoDriveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PicoDrive/PublicHeader.h>
+#import <PioDrive/PicodriveGameCore.h>
+#import <PioDrive/OESega32XSystemResponderClient.h>
