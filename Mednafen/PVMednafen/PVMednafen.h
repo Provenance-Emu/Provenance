05165bd1253175daa06b6ebd51a7d481f529d032
Joe Mattiello <jmattiello@hearst.com>
2016-12-20 12:09:48 -0500

    Update Mednafen module header and tvOS bundle name

--- Mednafen/PVMednafen/PVMednafen.h
+++ Mednafen/PVMednafen/PVMednafen.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double PVMednafenVersionNumber;
 FOUNDATION_EXPORT const unsigned char PVMednafenVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PVMednafen/PublicHeader.h>
-
+#import <PVMednafen/MednafenGameCore.h>
 
