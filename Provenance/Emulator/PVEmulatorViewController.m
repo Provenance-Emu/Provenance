b3ab3302e0cffd7608305073a7980b8722cb76bd
sonique <sonique6784@yahoo.fr>
2017-11-21 20:44:56 +1100

    moved Menu button to bottom left for iPhone X support

--- Provenance/Emulator/PVEmulatorViewController.m
+++ Provenance/Emulator/PVEmulatorViewController.m
@@ -22,6 +22,7 @@
 #import "PVEmulatorConfiguration.h"
 #import "PVControllerManager.h"
 #import "PViCade8BitdoController.h"
+#import "UIDevice+Hardware.h"
 
 @interface PVEmulatorViewController ()
 
@@ -184,7 +185,7 @@ void uncaughtExceptionHandler(NSException *exception)
 	
 	CGFloat alpha = [[PVSettingsModel sharedInstance] controllerOpacity];
 	self.menuButton = [UIButton buttonWithType:UIButtonTypeCustom];
-	[self.menuButton setFrame:CGRectMake(([[self view] bounds].size.width - 62) / 2, 10, 62, 22)];
+    [self.menuButton setFrame:CGRectMake( ([UIDevice isIphoneX] ? 10 : ([[self view] bounds].size.width - 62) / 2 ), ([UIDevice isIphoneX] ? 740 : 10), 62, 22)];
 	[self.menuButton setAutoresizingMask:UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin| UIViewAutoresizingFlexibleBottomMargin];
 	[self.menuButton setBackgroundImage:[UIImage imageNamed:@"button-thin"] forState:UIControlStateNormal];
 	[self.menuButton setBackgroundImage:[UIImage imageNamed:@"button-thin-pressed"] forState:UIControlStateHighlighted];
