4f3ab5c1a56ebc32c686fa3da581d5c394301d17
Joe Mattiello <git@joemattiello.com>
2018-02-17 01:17:26 -0500

    Fix Pico and ProSystem cores to build as modules
    Needed for inclusion in Swift code. Headers weren’t public.

--- PVProSystem/ProSystem/ProSystem.h
+++ PVProSystem/ProSystem/ProSystem.h
@@ -16,5 +16,5 @@ FOUNDATION_EXPORT const unsigned char ProSystemVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ProSystem/PublicHeader.h>
 
-#import <ProSystem/OE7800SystemResponderClient.h>
+// #import <ProSystem/OE7800SystemResponderClient.h>
 #import <ProSystem/ProSystemGameCore.h>
