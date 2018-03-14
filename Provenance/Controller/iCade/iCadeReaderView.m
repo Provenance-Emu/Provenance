a1f6bc867ea9316832f254d99bb8b5029a0973dd
Joe Mattiello <mail@joemattiello.com>
2018-03-14 01:25:12 -0400

    Fix warning fix on ios that broke tvos

--- Provenance/Controller/iCade/iCadeReaderView.m
+++ Provenance/Controller/iCade/iCadeReaderView.m
@@ -26,11 +26,13 @@
 static const char *ON_STATES_EN  = "wdxayhujikol";
 static const char *OFF_STATES_EN = "eczqtrfnmpgv";
 
-//static const char *ON_STATES_FR  = "zdxqyhujikol";
-//static const char *OFF_STATES_FR = "ecwatrfn,pgv";
-//
-//static const char *ON_STATES_DE  = "wdxazhujikol";
-//static const char *OFF_STATES_DE = "ecyqtrfnmpgv";
+#if TARGET_OS_TV
+static const char *ON_STATES_FR  = "zdxqyhujikol";
+static const char *OFF_STATES_FR = "ecwatrfn,pgv";
+
+static const char *ON_STATES_DE  = "wdxazhujikol";
+static const char *OFF_STATES_DE = "ecyqtrfnmpgv";
+#endif
 
 @interface iCadeReaderView() <UIKeyInput>
 
