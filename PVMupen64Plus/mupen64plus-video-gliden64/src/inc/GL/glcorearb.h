fa76a18b7c338861e2feab9194e1a4851a59cae4
Joe Mattiello <mail@joemattiello.com>
2018-03-27 20:08:31 -0400

    Fix gliden / mupen only arm64

--- PVMupen64Plus/mupen64plus-video-gliden64/src/inc/GL/glcorearb.h
+++ PVMupen64Plus/mupen64plus-video-gliden64/src/inc/GL/glcorearb.h
@@ -614,8 +614,8 @@ GLAPI void APIENTRY glBlendEquation (GLenum mode);
 #ifndef GL_VERSION_1_5
 #define GL_VERSION_1_5 1
 #include <stddef.h>
-typedef ptrdiff_t GLsizeiptr;
-typedef ptrdiff_t GLintptr;
+//typedef ptrdiff_t GLsizeiptr;
+//typedef ptrdiff_t GLintptr;
 #define GL_BUFFER_SIZE                    0x8764
 #define GL_BUFFER_USAGE                   0x8765
 #define GL_QUERY_COUNTER_BITS             0x8864
