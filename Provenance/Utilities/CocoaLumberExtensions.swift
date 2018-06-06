e8ac62ccacc8b38d5bf844c5a816f1ed284b5cc1
Joseph Mattiello <mail@joemattiello.com>
2018-06-05 22:39:00 -0400

    another logging crash fix

--- Provenance/Utilities/CocoaLumberExtensions.swift
+++ Provenance/Utilities/CocoaLumberExtensions.swift
@@ -81,7 +81,8 @@ public final class PVTTYFormatter : NSObject, DDLogFormatter {
             level = "WARNING "
         case .error:
             level = "ERROR "
-        case .off, .all:
+		default:
+//        case .off, .all:
             level = ""
         }
 
