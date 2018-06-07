23947ee5afc5c70c56f045bc3ca2e5f553fcf0c2
Joseph Mattiello <mail@joemattiello.com>
2018-06-06 21:11:07 -0400

    Refactor carthage script stage, speed up maybe
    use a build only on archive
    use a single script so i don’ tneed to keep edint project files

---
+++ Carthage/xcode.sh
@@ -0,0 +1,23 @@
+set -e
+
+if which xcodebuild > /dev/null; then
+    echo "Has XCode command line tools"
+else
+    echo "error: Missing XCode command line tools. Intall with 'xcode-select --install' from terminal then restart XCode."
+    osascript -e 'tell app "System Events" to display dialog "Error. Missing XCode command line tools. Intall with xcode-select --install from terminal then restart XCode." buttons {"OK"} with icon caution with title "Missing XCode command line tools"'
+    exit 1
+fi
+
+if which carthage >/dev/null; then
+    echo "Setting up Carthage"
+    /usr/local/bin/carthage bootstrap --no-use-binaries --cache-builds --platform $1 --project-directory "$SRCROOT"
+    /usr/local/bin/carthage outdated --xcode-warnings
+    if [ "${BUILD_STYLE}" == "Ad Hoc Distribution" ]
+    then
+      /usr/local/bin/carthage build --verbose --no-use-binaries --cache-builds --platform $1
+    fi
+else
+    echo "error: Carthage is not installed, download from https://github.com/Carthage/Carthage#installing-carthage"
+    osascript -e 'tell app "System Events" to display dialog "Error. Carthage is not installed, download from \nhttps://github.com/Carthage/Carthage#installing-carthage" buttons {"OK"} with icon caution with title "Missing Carthage"'
+    exit 1
+fi
