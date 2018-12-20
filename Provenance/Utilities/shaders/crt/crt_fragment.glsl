9b6c3ad551486d438f576b60756380bec57c08bf
MrJSchulte <30782821+mrjschulte@users.noreply.github.com>
2018-12-19 21:22:48 -0800

    Update crt_fragment.glsl
    Machine optimized fragment shader with the goal of reaching sub 16.6ms performance on the iPad Air A7 GPU (and slightly tweaked the look by lowering the black values).

--- Provenance/Utilities/shaders/crt/crt_fragment.glsl
+++ Provenance/Utilities/shaders/crt/crt_fragment.glsl
@@ -1,6 +1,6 @@
 // PUBLIC DOMAIN CRT SHADER
 //
-//   by Jay Mattis
+//   Originally by Jay Mattis, tweaked by MrJ
 //
 // I'm a big fan of Timothy Lottes' shader, but it doesn't scale well and I was looking for something that
 // was performant on my 4K TV and still looked decent on my phone. This takes a lot of inspiration from his
@@ -8,10 +8,19 @@
 // more on the input resolution rather than the output resolution (as long as the output resolution is
 // high enough).
 //
-// Left it unoptimized to show the theory behind the algorithm.
+// Left it unoptimized to show the theory behind the algorithm. (This is now no longer the case)
 //
 // It is an example what I personally would want as a display option for pixel art games.
 // Please take and use, change, or whatever.
+//
+// The original shader from Jay Mattis has been further machine optimized to allow for (sub) 16.6ms frame times 
+// resulting in stable 60fps CRT shader performance in Provenance on the original iPad Air (A7 GPU architecture 
+// from 2013(!!!). This optimization, together with the new Multi-threaded GL option in the 1.5 beta, 
+// should allow even the oldest iPad Air's to run the consoles of yesteryear at speed with full CRT shader effect. 
+// 
+// Other changes/comments
+// - Lowered the minimum black point set in the shader to 0.0015, down from 0.003.
+// - Couple this shader with the "Image Smoothing" option to reduce the anti-aliasing issues that could show up.
 
 #ifdef GL_ES
 precision mediump float;
@@ -22,124 +31,189 @@ precision mediump float;
 #endif
 
 varying highp vec2 fTexCoord;
-
 uniform vec4 DisplayRect;
 uniform sampler2D EmulatedImage;
 uniform vec2 EmulatedImageSize;
 uniform vec2 FinalRes;
-
-#define FINAL_RES FinalRes
-#define INPUT_SAMPLER EmulatedImage
-// These are to convert input texture coordinates to UV (0-1) space and back.
-#define INPUTCOORD_TO_UV( inputCoord ) ( inputCoord / DisplayRect.zw * EmulatedImageSize - DisplayRect.xy / DisplayRect.zw )
-#define UV_TO_INPUTCOORD( uv ) ( DisplayRect.xy / EmulatedImageSize + uv / EmulatedImageSize * DisplayRect.zw )
-
-#define USE_SCANLINES 1
-#define USE_SHADOWMASK 1
-
-#define BLOOM_AMOUNT 2.0
-#define MIN_BRIGHTNESS 0.003
-#define ROWS_OF_RESOLUTION 480.0
-#define SCANLINES_ALLOWED ( FINAL_RES.y >= ROWS_OF_RESOLUTION / 2.0 * 4.0 )
-#define SCANLINE_HARDNESS 4.0
-#define SCANLINE_MIN_BRIGHTNESS vec3( 0.25, 0.25, 0.25 )
-#define SHADOW_MASK_HARDNESS 16.0
-#define TVL 360.0
-#define WARP_EDGE_HARDNESS 256.0
-#define WARP_X ( 1.0 / 64.0 )
-#define WARP_Y ( 1.0 / 24.0 )
-
-float ToLinear1(float c){return(c<=0.04045)?c/12.92:pow((c+0.055)/1.055,2.4);}
-vec3 ToLinear(vec3 c){return c*c;}//return vec3(ToLinear1(c.r),ToLinear1(c.g),ToLinear1(c.b));}
-
-float ToSrgb1(float c){return(c<0.0031308?c*12.92:1.055*pow(c,0.41666)-0.055);}
-vec3 ToSrgb(vec3 c){return sqrt(c);}//return vec3(ToSrgb1(c.r),ToSrgb1(c.g),ToSrgb1(c.b));}
-
-highp vec2 Warp( highp vec2 uv )
+void main ()
 {
-    uv = uv * 2.0 - 1.0;
-    uv *= vec2( 1.0 + ( uv.y * uv.y ) * WARP_X, 1.0 + ( uv.x * uv.x ) * WARP_Y );
-    return uv * 0.5 + 0.5;
-}
-
-vec2 getShadowMaskRes()
-{
-    vec2 shadowMaskRes;
-    if ( FINAL_RES.y / 3.0 < TVL * 2.0 )
-    {
-        shadowMaskRes = FINAL_RES / 3.0;
-    }
-    else
-    {
-        shadowMaskRes = vec2( FINAL_RES.x / FINAL_RES.y * TVL, TVL );
-    }
-    return shadowMaskRes;
-}
-
-vec3 getShadowMaskRGB( highp vec2 uv )
-{
-#if USE_SHADOWMASK
-    vec2 shadowMaskRes = getShadowMaskRes();
-    highp vec2 pixelCoord = uv * shadowMaskRes * vec2( 3.0, 3.0 );
-    highp vec3 shadowMaskCoord = vec3( pixelCoord.x + 1.0, pixelCoord.x + 0.0, pixelCoord.x + 2.0 );
-    //vec3 shadowMaskRGB = clamp( abs( mod( shadowMaskCoord, 3.0 ) - 1.5 ) - 0.5, 0.0, 1.0 );
-    vec3 shadowMaskRGB = abs( mod( shadowMaskCoord, 3.0 ) - 1.5 ) / 1.5;
-    shadowMaskRGB = exp2( shadowMaskRGB * shadowMaskRGB * -SHADOW_MASK_HARDNESS );
-    return shadowMaskRGB;
-#else
-    return vec3(1.0);
-#endif
-}
-
-vec3 sampleRGB( highp vec2 uv, highp vec2 warpedUV )
-{
-    vec3 inputSample = ToLinear( texture( INPUT_SAMPLER, UV_TO_INPUTCOORD( warpedUV ) ).rgb );
-    
-    vec3 scanlineMultiplier = vec3( 1.0 );
-#if USE_SCANLINES
-    if ( SCANLINES_ALLOWED )
-    {
-        float scanlineY = mod( warpedUV.y, 2.0 / ROWS_OF_RESOLUTION ) / ( 2.0 / ROWS_OF_RESOLUTION );
-        float scanlineDistance = abs( scanlineY - 0.5 ) / 0.5;
-        float scanlineCoverage = exp2( scanlineDistance * scanlineDistance * -SCANLINE_HARDNESS );
-        scanlineMultiplier = mix( SCANLINE_MIN_BRIGHTNESS, vec3( 1.0 ), scanlineCoverage );
-    }
-#endif
-    
-    return max( inputSample * scanlineMultiplier, vec3( MIN_BRIGHTNESS ) ) * getShadowMaskRGB( uv );
-}
-
-vec3 sampleRow( highp vec2 uv, vec3 centerTap )
-{
-    highp vec2 leftUV = uv + vec2( -1.0 / FINAL_RES.x, 0.0 );
-    highp vec2 rightUV = uv + vec2( 1.0 / FINAL_RES.x, 0.0 );
-    return centerTap * 0.5
-    + sampleRGB( leftUV, Warp( leftUV ) ) * 0.25
-    + sampleRGB( rightUV, Warp( rightUV ) ) * 0.25;
-}
-
-vec3 sampleCol( highp vec2 uv, vec3 centerTap )
-{
-    return sampleRow( uv, centerTap );
-}
-
-vec3 crtFilter( highp vec2 uv )
-{
-    highp vec2 warpedUV = Warp( uv );
-    float edgeMask = 1.0 - exp2( ( 1.0 - max( abs( warpedUV.x - 0.5 ), abs( warpedUV.y - 0.5 ) ) / 0.5 ) * -WARP_EDGE_HARDNESS );
-    float bloomAmount = BLOOM_AMOUNT;
-#if USE_SCANLINES
-    if ( SCANLINES_ALLOWED )
-    {
-        bloomAmount *= 2.0;
-    }
-#endif
-    vec3 centerTap = sampleRGB( uv, warpedUV );
-    return ToSrgb( ( centerTap + sampleCol( uv, centerTap ) * bloomAmount ) * edgeMask );
-}
-
-void main( void )
-{
-    gl_FragColor.rgb = crtFilter( INPUTCOORD_TO_UV( fTexCoord ) );
-    gl_FragColor.a = 1.0;
+    highp vec2 uv_1;
+    uv_1 = (((fTexCoord / DisplayRect.zw) * EmulatedImageSize) - (DisplayRect.xy / DisplayRect.zw));
+    float bloomAmount_2;
+    highp vec2 tmpvar_3;
+    highp vec2 uv_4;
+    uv_4 = ((uv_1 * 2.0) - 1.0);
+    highp vec2 tmpvar_5;
+    tmpvar_5.x = (1.0 + ((uv_4.y * uv_4.y) * 0.015625));
+    tmpvar_5.y = (1.0 + ((uv_4.x * uv_4.x) * 0.04166667));
+    uv_4 = (uv_4 * tmpvar_5);
+    tmpvar_3 = ((uv_4 * 0.5) + 0.5);
+    highp float tmpvar_6;
+    tmpvar_6 = (1.0 - exp2((
+                            (1.0 - (max (abs(
+                                             (tmpvar_3.x - 0.5)
+                                             ), abs(
+                                                    (tmpvar_3.y - 0.5)
+                                                    )) / 0.5))
+                            * -256.0)));
+    bloomAmount_2 = 2.0;
+    if ((FinalRes.y >= 960.0)) {
+        bloomAmount_2 = 4.0;
+    };
+    highp vec3 tmpvar_7;
+    highp vec3 scanlineMultiplier_8;
+    lowp vec4 tmpvar_9;
+    highp vec2 P_10;
+    P_10 = ((DisplayRect.xy / EmulatedImageSize) + ((tmpvar_3 / EmulatedImageSize) * DisplayRect.zw));
+    tmpvar_9 = texture2D (EmulatedImage, P_10);
+    lowp vec3 tmpvar_11;
+    tmpvar_11 = (tmpvar_9.xyz * tmpvar_9.xyz);
+    scanlineMultiplier_8 = vec3(1.0, 1.0, 1.0);
+    if ((FinalRes.y >= 960.0)) {
+        highp float tmpvar_12;
+        tmpvar_12 = (abs((
+                          ((float(mod (tmpvar_3.y, 0.004166667))) / 0.004166667)
+                          - 0.5)) / 0.5);
+        scanlineMultiplier_8 = mix (vec3(0.25, 0.25, 0.25), vec3(1.0, 1.0, 1.0), exp2((
+                                                                                       (tmpvar_12 * tmpvar_12)
+                                                                                       * -4.0)));
+    };
+    highp vec3 tmpvar_13;
+    tmpvar_13 = max ((tmpvar_11 * scanlineMultiplier_8), vec3(0.0015, 0.0015, 0.0015));
+    vec2 shadowMaskRes_14;
+    if (((FinalRes.y / 3.0) < 720.0)) {
+        shadowMaskRes_14 = (FinalRes / 3.0);
+    } else {
+        vec2 tmpvar_15;
+        tmpvar_15.y = 360.0;
+        tmpvar_15.x = ((FinalRes.x / FinalRes.y) * 360.0);
+        shadowMaskRes_14 = tmpvar_15;
+    };
+    highp vec2 tmpvar_16;
+    tmpvar_16 = ((uv_1 * shadowMaskRes_14) * vec2(3.0, 3.0));
+    highp vec3 tmpvar_17;
+    tmpvar_17.x = (tmpvar_16.x + 1.0);
+    tmpvar_17.y = tmpvar_16.x;
+    tmpvar_17.z = (tmpvar_16.x + 2.0);
+    highp vec3 tmpvar_18;
+    tmpvar_18 = (abs((
+                      (vec3(mod (tmpvar_17, 3.0)))
+                      - 1.5)) / 1.5);
+    tmpvar_7 = (tmpvar_13 * exp2((
+                                  (tmpvar_18 * tmpvar_18)
+                                  * -16.0)));
+    vec2 tmpvar_19;
+    tmpvar_19.y = 0.0;
+    tmpvar_19.x = (-1.0 / FinalRes.x);
+    highp vec2 tmpvar_20;
+    tmpvar_20 = (uv_1 + tmpvar_19);
+    vec2 tmpvar_21;
+    tmpvar_21.y = 0.0;
+    tmpvar_21.x = (1.0/(FinalRes.x));
+    highp vec2 tmpvar_22;
+    tmpvar_22 = (uv_1 + tmpvar_21);
+    highp vec2 tmpvar_23;
+    highp vec2 uv_24;
+    uv_24 = ((tmpvar_20 * 2.0) - 1.0);
+    highp vec2 tmpvar_25;
+    tmpvar_25.x = (1.0 + ((uv_24.y * uv_24.y) * 0.015625));
+    tmpvar_25.y = (1.0 + ((uv_24.x * uv_24.x) * 0.04166667));
+    uv_24 = (uv_24 * tmpvar_25);
+    tmpvar_23 = ((uv_24 * 0.5) + 0.5);
+    highp vec3 tmpvar_26;
+    highp vec3 scanlineMultiplier_27;
+    lowp vec4 tmpvar_28;
+    highp vec2 P_29;
+    P_29 = ((DisplayRect.xy / EmulatedImageSize) + ((tmpvar_23 / EmulatedImageSize) * DisplayRect.zw));
+    tmpvar_28 = texture2D (EmulatedImage, P_29);
+    lowp vec3 tmpvar_30;
+    tmpvar_30 = (tmpvar_28.xyz * tmpvar_28.xyz);
+    scanlineMultiplier_27 = vec3(1.0, 1.0, 1.0);
+    if ((FinalRes.y >= 960.0)) {
+        highp float tmpvar_31;
+        tmpvar_31 = (abs((
+                          ((float(mod (tmpvar_23.y, 0.004166667))) / 0.004166667)
+                          - 0.5)) / 0.5);
+        scanlineMultiplier_27 = mix (vec3(0.25, 0.25, 0.25), vec3(1.0, 1.0, 1.0), exp2((
+                                                                                        (tmpvar_31 * tmpvar_31)
+                                                                                        * -4.0)));
+    };
+    highp vec3 tmpvar_32;
+    tmpvar_32 = max ((tmpvar_30 * scanlineMultiplier_27), vec3(0.0015, 0.0015, 0.0015));
+    vec2 shadowMaskRes_33;
+    if (((FinalRes.y / 3.0) < 720.0)) {
+        shadowMaskRes_33 = (FinalRes / 3.0);
+    } else {
+        vec2 tmpvar_34;
+        tmpvar_34.y = 360.0;
+        tmpvar_34.x = ((FinalRes.x / FinalRes.y) * 360.0);
+        shadowMaskRes_33 = tmpvar_34;
+    };
+    highp vec2 tmpvar_35;
+    tmpvar_35 = ((tmpvar_20 * shadowMaskRes_33) * vec2(3.0, 3.0));
+    highp vec3 tmpvar_36;
+    tmpvar_36.x = (tmpvar_35.x + 1.0);
+    tmpvar_36.y = tmpvar_35.x;
+    tmpvar_36.z = (tmpvar_35.x + 2.0);
+    highp vec3 tmpvar_37;
+    tmpvar_37 = (abs((
+                      (vec3(mod (tmpvar_36, 3.0)))
+                      - 1.5)) / 1.5);
+    tmpvar_26 = (tmpvar_32 * exp2((
+                                   (tmpvar_37 * tmpvar_37)
+                                   * -16.0)));
+    highp vec2 tmpvar_38;
+    highp vec2 uv_39;
+    uv_39 = ((tmpvar_22 * 2.0) - 1.0);
+    highp vec2 tmpvar_40;
+    tmpvar_40.x = (1.0 + ((uv_39.y * uv_39.y) * 0.015625));
+    tmpvar_40.y = (1.0 + ((uv_39.x * uv_39.x) * 0.04166667));
+    uv_39 = (uv_39 * tmpvar_40);
+    tmpvar_38 = ((uv_39 * 0.5) + 0.5);
+    highp vec3 scanlineMultiplier_41;
+    lowp vec4 tmpvar_42;
+    highp vec2 P_43;
+    P_43 = ((DisplayRect.xy / EmulatedImageSize) + ((tmpvar_38 / EmulatedImageSize) * DisplayRect.zw));
+    tmpvar_42 = texture2D (EmulatedImage, P_43);
+    lowp vec3 tmpvar_44;
+    tmpvar_44 = (tmpvar_42.xyz * tmpvar_42.xyz);
+    scanlineMultiplier_41 = vec3(1.0, 1.0, 1.0);
+    if ((FinalRes.y >= 960.0)) {
+        highp float tmpvar_45;
+        tmpvar_45 = (abs((
+                          ((float(mod (tmpvar_38.y, 0.004166667))) / 0.004166667)
+                          - 0.5)) / 0.5);
+        scanlineMultiplier_41 = mix (vec3(0.25, 0.25, 0.25), vec3(1.0, 1.0, 1.0), exp2((
+                                                                                        (tmpvar_45 * tmpvar_45)
+                                                                                        * -4.0)));
+    };
+    highp vec3 tmpvar_46;
+    tmpvar_46 = max ((tmpvar_44 * scanlineMultiplier_41), vec3(0.0015, 0.0015, 0.0015));
+    vec2 shadowMaskRes_47;
+    if (((FinalRes.y / 3.0) < 720.0)) {
+        shadowMaskRes_47 = (FinalRes / 3.0);
+    } else {
+        vec2 tmpvar_48;
+        tmpvar_48.y = 360.0;
+        tmpvar_48.x = ((FinalRes.x / FinalRes.y) * 360.0);
+        shadowMaskRes_47 = tmpvar_48;
+    };
+    highp vec2 tmpvar_49;
+    tmpvar_49 = ((tmpvar_22 * shadowMaskRes_47) * vec2(3.0, 3.0));
+    highp vec3 tmpvar_50;
+    tmpvar_50.x = (tmpvar_49.x + 1.0);
+    tmpvar_50.y = tmpvar_49.x;
+    tmpvar_50.z = (tmpvar_49.x + 2.0);
+    highp vec3 tmpvar_51;
+    tmpvar_51 = (abs((
+                      (vec3(mod (tmpvar_50, 3.0)))
+                      - 1.5)) / 1.5);
+    highp vec3 tmpvar_52;
+    tmpvar_52 = sqrt(((tmpvar_7 +
+                       ((((tmpvar_7 * 0.5) + (tmpvar_26 * 0.25)) + ((tmpvar_46 *
+                                                                     exp2(((tmpvar_51 * tmpvar_51) * -16.0))
+                                                                     ) * 0.25)) * bloomAmount_2)
+                       ) * tmpvar_6));
+    gl_FragColor.xyz = tmpvar_52;
+    gl_FragColor.w = 1.0;
 }
