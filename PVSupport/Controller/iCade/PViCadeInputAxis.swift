13af8ca63eb5d71b487805e38a2ff4cdc3286ae2
Joseph Mattiello <mail@joemattiello.com>
2019-09-09 00:43:50 -0400

    Update carts, xcode 11 supported branches
    Signed-off-by: Joseph Mattiello <mail@joemattiello.com>

--- PVSupport/Controller/iCade/PViCadeInputAxis.swift
+++ PVSupport/Controller/iCade/PViCadeInputAxis.swift
@@ -10,10 +10,12 @@ import GameController
 
 public final class PViCadeAxisInput: GCControllerAxisInput {
     private var _value: Float = 0.0
+
     public override var value: Float {
         get {
             return _value
         }
+        @objc(setButtonValue:)
         set {
             _value = newValue
         }
