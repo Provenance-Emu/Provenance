f2ae8a59a1889b602d0132beaf13fc54d7fe9087
MrJs <30782821+mrjschulte@users.noreply.github.com>
2020-09-21 21:25:22 -0700

    Update PVGLViewController.m
    Per the docs PreferredFPS takes an int, and generally only allows for frame rates of 60,30,15,etc. 
    Setting this to 60 seems to resolve a lot of the frame skipping issues we've seen on modern devices, especially on Displays that can refresh >60.

--- Provenance/Emulator/PVGLViewController.m
+++ Provenance/Emulator/PVGLViewController.m
@@ -231,12 +231,7 @@ struct RenderSettings {
 }
 
 - (void) updatePreferredFPS {
-    float preferredFPS = self.emulatorCore.frameInterval;
-    if (preferredFPS  < 10) {
-        WLOG(@"Cores frame interval (%f) too low. Setting to 60", preferredFPS);
-        preferredFPS = 60;
-    }
-
+    int preferredFPS = 60;
     [self setPreferredFramesPerSecond:preferredFPS];
 }
 
