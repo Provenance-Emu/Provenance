e49123181261b20d5dc21de5b6eb475870242ea3
MrJs <30782821+mrjschulte@users.noreply.github.com>
2020-09-26 13:46:11 -0700

    Update OEGameAudio.m
    Fix spelling error

--- PVSupport/PVSupport/Audio/OEGameAudio.m
+++ PVSupport/PVSupport/Audio/OEGameAudio.m
@@ -283,7 +283,7 @@ OSStatus RenderCallback(void                       *in,
 
         err = AUGraphConnectNodeInput(mGraph, mConverterNode, 0, mMixerNode, i);
         if(err) { ELOG(@"Couldn't connect the converter to the mixer"); }
-        else { ELOG(@"Conncted the converter to the mixer"); }
+        else { ELOG(@"Connected the converter to the mixer"); }
     }
     // connect the player to the output unit (stream format will propagate)
          
@@ -291,7 +291,7 @@ OSStatus RenderCallback(void                       *in,
     if(err) {
         ELOG(@"Could not connect the input of the output");
     } {
-        DLOG(@"Conncted input of the output");
+        DLOG(@"Connected input of the output");
     }
     
     //AudioUnitSetParameter(mOutputUnit, kAudioUnitParameterUnit_LinearGain, kAudioUnitScope_Global, 0, [[[GameDocumentController sharedDocumentController] preferenceController] volume] ,0);
