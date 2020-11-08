40b46cb23f04120ecf84e2319a4f231d81ca3a44
MrJs <30782821+mrjschulte@users.noreply.github.com>
2020-11-07 18:31:54 -0800

    Update PVGameLibraryViewController+CollectionView.swift
    Pad out space around Tiles a little more.

--- Provenance/Game Library/UI/Game Library/CollectionViewController/PVGameLibraryViewController+CollectionView.swift
+++ Provenance/Game Library/UI/Game Library/CollectionViewController/PVGameLibraryViewController+CollectionView.swift
@@ -109,7 +109,7 @@ extension PVGameLibraryViewController: UICollectionViewDelegateFlowLayout {
 
     func collectionView(_ collectionView: UICollectionView, layout _: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         #if os(tvOS)
-            return UIEdgeInsets(top: 15, left: 10, bottom: 5, right: 10)
+            return UIEdgeInsets(top: 20, left: 20, bottom: 30, right: 20)
         #else
         let item: Section.Item? = firstModel(in: collectionView, at: section)
         switch item {
