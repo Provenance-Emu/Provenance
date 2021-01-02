c40d9781fe7ec0800161d6e2eed7e13d708bf85f
Dave Nicolson <david.nicolson@gmail.com>
2021-01-02 20:10:12 +0100

    Make collapse buttons focusable on tvOS (#1467)
    * Make collapse buttons focusable
    
    * Change header and collapse button style
    
    * Only make collapsable headers focusable
    
    * Make header and collapse button different focuses
    
    * Move focus from button back to header

--- Provenance/Game Library/UI/Game Library/CollectionViewController/PVGameLibraryViewController+CollectionView.swift
+++ Provenance/Game Library/UI/Game Library/CollectionViewController/PVGameLibraryViewController+CollectionView.swift
@@ -82,8 +82,10 @@ extension PVGameLibraryViewController: UICollectionViewDelegateFlowLayout {
 
     #if os(tvOS)
         func collectionView(_ collectionView: UICollectionView, layout _: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-            let item: Section.Item = try! collectionView.rx.model(at: IndexPath(item: 0, section: section))
+            let item: Section.Item? = firstModel(in: collectionView, at: section)
             switch item {
+            case .none:
+                return .zero
             case .game:
                 return 40
             case .saves, .favorites, .recents:
