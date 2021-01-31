314a9fb32e75e6e975597a3b7324af1f1eb10231
MrJs <30782821+mrjschulte@users.noreply.github.com>
2021-01-30 18:06:22 -0800

    Enables 8BitDo M30 Mapping for Sega games
    This remaps the MFI Extended controllers to better fit to a 8BitDo M30 for use with the Genesis/Megadrive/Sega/MegaCD system inside of Provenance.
    
    All of the buttons on the M30 map 1:1 to where they would be on the 6 Button Sega Controller, it also moves the Start button from the Extended Controller to the Start button on the M30. The L and R triggers are NOT used and enabled.

--- Cores/Genesis-Plus-GX/PVGenesis/Genesis/PVGenesisEmulatorCore.m
+++ Cores/Genesis-Plus-GX/PVGenesis/Genesis/PVGenesisEmulatorCore.m
@@ -724,7 +724,34 @@ static bool environment_callback(unsigned cmd, void *data)
         if ([controller extendedGamepad]) {
             GCExtendedGamepad *gamepad = [controller extendedGamepad];
             GCControllerDirectionPad *dpad = [gamepad dpad];
-            switch (buttonID) {
+            if (PVSettingsModel.shared.use8BitdoM30) // Maps the Sega Controls to the 8BitDo M30 if enabled in Settings / Controller
+            {switch (buttonID) {
+                case PVGenesisButtonUp:
+                    return [[[gamepad leftThumbstick] up] value] > 0.5;
+                case PVGenesisButtonDown:
+                    return [[[gamepad leftThumbstick] down] value] > 0.5;
+                case PVGenesisButtonLeft:
+                    return [[[gamepad leftThumbstick] left] value] > 0.5;
+                case PVGenesisButtonRight:
+                    return [[[gamepad leftThumbstick] right] value] > 0.5;
+                case PVGenesisButtonA:
+                    return [[gamepad buttonA] isPressed];
+                case PVGenesisButtonB:
+                    return [[gamepad buttonB] isPressed];
+                case PVGenesisButtonC:
+                    return [[gamepad rightShoulder] isPressed];
+                case PVGenesisButtonX:
+                    return [[gamepad buttonX] isPressed];
+                case PVGenesisButtonY:
+                    return [[gamepad buttonY] isPressed];
+                case PVGenesisButtonZ:
+                    return [[gamepad leftShoulder] isPressed];
+                case PVGenesisButtonStart:
+                    return [[gamepad buttonMenu] isPressed];
+                default:
+                    break;
+            }}
+            { switch (buttonID) {
                 case PVGenesisButtonUp:
                     return [[dpad up] isPressed]?:[[[gamepad leftThumbstick] up] isPressed];
                 case PVGenesisButtonDown:
@@ -748,8 +775,8 @@ static bool environment_callback(unsigned cmd, void *data)
                 case PVGenesisButtonStart:
                     return [[gamepad rightTrigger] isPressed];
                 default:
-                    break;
-            }
+                   break;
+            }}
             
         } else if ([controller gamepad]) {
             GCGamepad *gamepad = [controller gamepad];
