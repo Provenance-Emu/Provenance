57fee2c0377366092af495d27903824b02b8a5a3
Joseph Mattello <mail@joemattiello.com>
2021-12-16 01:29:06 -0500

    Fix tvOS target
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- ProvenanceTV-Bridging-Header.h
+++ ProvenanceTV-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 #import "PVLogViewController.h"
-#import "PVAltKitService.h"
+//#import "PVAltKitService.h"
 
 // -----------------------------------------------------------------------------
 // Begin Swiftify generated imports
