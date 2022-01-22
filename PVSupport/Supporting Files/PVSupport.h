36c553dcae49607e40f36b6ce1d583f1b8aa17dd
Joseph Mattiello <mail@joemattiello.com>
2022-01-22 09:10:31 -0500

    pvsupport fix debugger by removing SwiftPM header

--- PVSupport/Supporting Files/PVSupport.h
+++ PVSupport/Supporting Files/PVSupport.h
@@ -20,26 +20,26 @@ FOUNDATION_EXPORT double PVSupport_FrameworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char PVSupport_FrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PVSupport/PublicHeader.h>
-#if SWIFT_PACKAGE
-    #import <PVSupportObjC/DebugUtils.h>
-    #import <PVSupportObjC/PVEmulatorCore.h>
-    #import <PVSupportObjC/NSObject+PVAbstractAdditions.h>
-    #import <PVSupportObjC/NSFileManager+OEHashingAdditions.h>
-    #import <PVSupportObjC/PVLogging.h>
-    #import <PVSupportObjC/PVLogEntry.h>
-    #import <PVSupportObjC/PVProvenanceLogging.h>
-
-    # pragma mark - Audio
-    #import <PVSupportObjC/TPCircularBuffer.h>
-    #import <PVSupportObjC/OERingBuffer.h>
-    #import <PVSupportObjC/OEGameAudio.h>
-    #ifdef __cplusplus
-    #import <PVSupportObjC/CARingBuffer.h>
-    //#import <PVSupport/CAAtomic.h>
-    #import <PVSupportObjC/CAAudioTimeStamp.h>
-    #endif
-    #import <PVSupportObjC/OEGeometry.h>
-#else
+//#if SWIFT_PACKAGE
+//    #import <PVSupportObjC/DebugUtils.h>
+//    #import <PVSupportObjC/PVEmulatorCore.h>
+//    #import <PVSupportObjC/NSObject+PVAbstractAdditions.h>
+//    #import <PVSupportObjC/NSFileManager+OEHashingAdditions.h>
+//    #import <PVSupportObjC/PVLogging.h>
+//    #import <PVSupportObjC/PVLogEntry.h>
+//    #import <PVSupportObjC/PVProvenanceLogging.h>
+//
+//    # pragma mark - Audio
+//    #import <PVSupportObjC/TPCircularBuffer.h>
+//    #import <PVSupportObjC/OERingBuffer.h>
+//    #import <PVSupportObjC/OEGameAudio.h>
+//    #ifdef __cplusplus
+//    #import <PVSupportObjC/CARingBuffer.h>
+//    //#import <PVSupport/CAAtomic.h>
+//    #import <PVSupportObjC/CAAudioTimeStamp.h>
+//    #endif
+//    #import <PVSupportObjC/OEGeometry.h>
+//#else
     #import <PVSupport/DebugUtils.h>
     #import <PVSupport/PVEmulatorCore.h>
     #import <PVSupport/NSObject+PVAbstractAdditions.h>
@@ -58,5 +58,5 @@ FOUNDATION_EXPORT const unsigned char PVSupport_FrameworkVersionString[];
     #import <PVSupport/CAAudioTimeStamp.h>
     #endif
     #import <PVSupport/OEGeometry.h>
-#endif
+//#endif
 
