6b9ab8a4ad42d70b43e3cf450a7e5077041ddad1
Joseph Mattello <mail@joemattiello.com>
2022-01-22 00:32:31 -0500

    Add PVSnesticle core
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

---
+++ Cores/snesticle/PVSnesticle/Core/PVSnesticleCore.swift
@@ -0,0 +1,29 @@
+//
+//  PVSnesticleCore.swift
+//  PVSNES
+//
+
+import PVSupport
+import UIKit
+
+extension PVSnesticleCore: GameWithCheat {
+    public func setCheat(
+        code: String,
+        type: String,
+        enabled: Bool
+    ) -> Bool
+    {
+        do {
+            try self.setCheat(code, setType: type, setEnabled: enabled)
+            return true
+        } catch let error {
+            NSLog("Error setCheat \(error)")
+            return false
+        }
+    }
+    
+    public func supportsCheatCode() -> Bool
+    {
+        return true
+    }
+}
---
+++ Cores/snesticle/snesticle-tvOS/snesticle_tvOS.swift
@@ -0,0 +1,11 @@
+//
+//  snesticle_tvOS.swift
+//  snesticle-tvOS
+//
+//  Created by Joseph Mattiello on 1/22/22.
+//  Copyright © 2022 Provenance Emu. All rights reserved.
+//
+
+class snesticle_tvOS {
+
+}
