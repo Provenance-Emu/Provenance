e05e24720cd3bf385bd04a7aebaf7b699a5dd8b6
Joseph Mattello <mail@joemattiello.com>
2022-01-24 00:46:35 -0500

    CoreOptional placeholder for pure swift
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- PVSupport/Sources/PVSupport/CoreOptions/Protocols/CoreOptional.swift
+++ PVSupport/Sources/PVSupport/CoreOptions/Protocols/CoreOptional.swift
@@ -8,6 +8,33 @@
 
 import Foundation
 
-public protocol CoreOptional: AnyObject {
+public protocol CoreOptional where Self: PVEmulatorCore {
+    @nonobjc
     static var options: [CoreOption] { get }
+    
+//    static func bool(forOption option: String) -> Bool
+//    static func int(forOption option: String) -> Int
+//    static func float(forOption option: String) -> Float
+//    static func string(forOption option: String) -> String?
 }
+
+//public extension CoreOptional {
+//    static func bool(forOption option: String) -> Bool {
+//        return valueForOption(Bool.self, option) ?? false
+//    }
+//
+//    static func int(forOption option: String) -> Int {
+//        let value = valueForOption(Int.self, option)
+//        return value ?? 0
+//    }
+//
+//    static func float(forOption option: String) -> Float {
+//        let value = valueForOption(Float.self, option)
+//        return value ?? 0
+//    }
+//
+//    static func string(forOption option: String) -> String? {
+//        let value = valueForOption(String.self, option)
+//        return value
+//    }
+//}
