6e38ff6bd0d2953e7d09011d25eb97a2056c5f24
ToddLa <ToddLa@HotMail.com>
2022-01-31 06:57:24 -0800

    support TVAlert on iOS

--- ProvenanceTV/TVAlertController.swift
+++ ProvenanceTV/TVAlertController.swift
@@ -10,6 +10,28 @@
 
 import UIKit
 
+// these are the defaults assuming Dark mode, etc.
+private let _modalPresentationStyle = UIModalPresentationStyle.overFullScreen  // default to .overFullScreen OR .blurOverFullScreen
+private let _fullscreenColor = UIColor.black.withAlphaComponent(0.8)
+private let _backgroundColor = UIColor(red:0.11, green:0.11, blue:0.12, alpha:1)      // secondarySystemGroupedBackground
+private let _defaultButtonColor = UIColor(white: 0.222, alpha: 0.666)
+private let _destructiveButtonColor = UIColor.systemRed.withAlphaComponent(0.666)
+private let _blur = true
+private let _borderWidth = 4.0
+private let _fontTitleF = 1.5
+private let _animateDuration = 0.200
+
+// os specific defaults
+#if os(tvOS)
+    private let _font = UIFont.systemFont(ofSize: 48.0)
+    private let _inset:CGFloat = 32.0
+    private let _maxWidthF:CGFloat = 0.50
+#else
+    private let _font = UIFont.preferredFont(forTextStyle: .body)
+    private let _inset:CGFloat = 16.0
+    private let _maxWidthF:CGFloat = 0.80
+#endif
+
 protocol UIAlertControllerProtocol : UIViewController {
 
      func addAction(_ action: UIAlertAction)
@@ -25,18 +47,19 @@ protocol UIAlertControllerProtocol : UIViewController {
      var preferredStyle: UIAlertController.Style { get }
 }
 
-// take over the UIAlertController initializer and return our class sometimes....
+// take over (aka mock) the UIAlertController initializer and return our class sometimes....
 func UIAlertController(title: String?, message: String?, preferredStyle style: UIAlertController.Style) -> UIAlertControllerProtocol {
     #if os(tvOS)
         return TVAlertController.init(title:title, message: message, preferredStyle: style)
     #else
-        if style == .actionSheet {
+        if style == .alert {
+            // always use system Alert on iOS
             return UIAlertController.init(title:title, message: message, preferredStyle: style)
-            //return TVAlertController.init(title:title, message: message, preferredStyle: style)
         }
         else {
-            return UIAlertController.init(title:title, message: message, preferredStyle: style)
-            //return TVAlertController.init(title:title, message: message, preferredStyle: style)
+            // maybe use custom ActionSheet
+            //return UIAlertController.init(title:title, message: message, preferredStyle: style)
+            return TVAlertController.init(title:title, message: message, preferredStyle: style)
         }
     #endif
 }
@@ -44,7 +67,7 @@ func UIAlertController(title: String?, message: String?, preferredStyle style: U
 extension UIAlertController : UIAlertControllerProtocol { }
 
 class TVAlertController: UIViewController, UIAlertControllerProtocol {
-    
+
     var preferredStyle = UIAlertController.Style.alert
     var actions = [UIAlertAction]()
     var textFields:[UITextField]?
@@ -53,20 +76,12 @@ class TVAlertController: UIViewController, UIAlertControllerProtocol {
     
     var autoDismiss = true          // a UIAlertController is always autoDismiss
 
-    // these are the PV defaults assuming Dark mode, etc.
-    private let _fullscreenColor = UIColor.black.withAlphaComponent(0.8)
-    private let _blur = false
-    private let _backgroundColor = UIColor(red:0.11, green:0.11, blue:0.12, alpha:1)      // secondarySystemGroupedBackground
-    private let _borderWidth = 4.0
-    private let _insets = UIEdgeInsets(top:32, left:32, bottom:32, right:32)
-    private let _maxWidth = (UIApplication.shared.keyWindow?.bounds.width ?? UIScreen.main.bounds.width)  * 0.80
-
     private let stack = {() -> UIStackView in
         let stack = UIStackView(arrangedSubviews: [UILabel(), UILabel()])
         stack.axis = .vertical
         stack.distribution = .fill
         stack.alignment = .fill
-        stack.spacing = 8
+        stack.spacing = floor(_font.lineHeight / 4.0)
         return stack
     }()
 
@@ -74,10 +89,10 @@ class TVAlertController: UIViewController, UIAlertControllerProtocol {
     
     convenience init(title: String?, message: String?, preferredStyle: UIAlertController.Style) {
         self.init(nibName:nil, bundle: nil)
-        self.preferredStyle = preferredStyle
         self.title = title
         self.message = message
-        self.modalPresentationStyle = .overFullScreen    // default to .overFullScreen OR .blurOverFullScreen
+        self.preferredStyle = preferredStyle
+        self.modalPresentationStyle = _modalPresentationStyle
         self.modalTransitionStyle = .crossDissolve
     }
     
@@ -90,9 +105,9 @@ class TVAlertController: UIViewController, UIAlertControllerProtocol {
         }
     }
     
-    var font = UIFont.preferredFont(forTextStyle: .body) {
+    var font = _font {
         didSet {
-            spacing = floor(font.lineHeight / 8.0);
+             spacing = floor(font.lineHeight / 4.0);
         }
     }
     
@@ -102,14 +117,10 @@ class TVAlertController: UIViewController, UIAlertControllerProtocol {
         set {
             let label = stack.arrangedSubviews[0] as! UILabel
             label.text = newValue
-            #if os(iOS)
-                label.font = .preferredFont(forTextStyle: .headline)
-            #else
-                label.font = .boldSystemFont(ofSize: font.pointSize * 1.5)
-            #endif
+            label.font = .boldSystemFont(ofSize: font.pointSize * _fontTitleF)
             label.numberOfLines = 0
             label.textAlignment = .center
-            label.preferredMaxLayoutWidth = _maxWidth
+            label.preferredMaxLayoutWidth = maxWidth
         }
         get {
             return (stack.arrangedSubviews[0] as? UILabel)?.text
@@ -123,7 +134,7 @@ class TVAlertController: UIViewController, UIAlertControllerProtocol {
             label.font = self.font
             label.numberOfLines = 0
             label.textAlignment = .center
-            label.preferredMaxLayoutWidth = _maxWidth
+            label.preferredMaxLayoutWidth = maxWidth
         }
         get {
             return (stack.arrangedSubviews[1] as? UILabel)?.text
@@ -176,25 +187,29 @@ class TVAlertController: UIViewController, UIAlertControllerProtocol {
             doubleStack()
         }
         
-        let back = UIView()
-        back.addSubview(stack)
-        view.addSubview(back)
+        let menu = UIView()
+        menu.addSubview(stack)
+        view.addSubview(menu)
 
-        back.layer.cornerRadius = min(_insets.top, _insets.left)
-        back.clipsToBounds = back.layer.cornerRadius != 0.0
-        back.backgroundColor = _backgroundColor
-        
-        if _borderWidth != 0.0 {
-            back.layer.borderWidth = _borderWidth
-            back.layer.borderColor = UIApplication.shared.keyWindow?.tintColor.cgColor
-        }
+        menu.layer.cornerRadius = _inset
+        menu.backgroundColor = _backgroundColor
+        view.backgroundColor = isFullscreen ? _fullscreenColor : nil
 
         if _blur {
             let blur = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
             blur.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            blur.frame = back.bounds;
-            back.addSubview(blur)
-            back.sendSubviewToBack(blur)
+            blur.frame = menu.bounds;
+            blur.layer.cornerRadius = _inset
+            blur.layer.masksToBounds = true
+            menu.insertSubview(blur, at: 0)
+            menu.backgroundColor = nil
+        }
+        
+        // use shape layer to mask out the area under the menu so the effect view can blur it
+        if _blur && isFullscreen {
+            view.backgroundColor = nil
+            // add a CAShapeLayer to the menu so we can animate it in-out
+            menu.layer.insertSublayer(CAShapeLayer(), at:0)
         }
     }
     
@@ -220,52 +235,131 @@ class TVAlertController: UIViewController, UIAlertControllerProtocol {
             stack.insertArrangedSubview(hstack, at:2+i)
          }
     }
+    
+    // MARK: layout and size
+    
+    private var isFullscreen: Bool {
+        return (view.window ?? UIApplication.shared.keyWindow)?.bounds == view.bounds || modalPresentationStyle == .overFullScreen
+    }
 
+    private var maxWidth: CGFloat {
+        return (UIApplication.shared.keyWindow?.bounds.width ?? UIScreen.main.bounds.width) * _maxWidthF
+    }
+
+    override var preferredContentSize: CGSize {
+        get {
+            var size = stack.systemLayoutSizeFitting(.zero)
+            print(size)
+            size.width = min(size.width, maxWidth)
+            print(size)
+            if size != .zero {
+                size.width  += (_inset * 2)
+                size.height += (_inset * 2)
+            }
+            return size
+        }
+        set {
+            super.preferredContentSize = newValue
+        }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        guard let content = view.subviews.last else {return}
+        
+        let rect = CGRect(origin: .zero, size: self.preferredContentSize)
+        stack.frame = rect.inset(by: UIEdgeInsets(top:_inset, left:_inset, bottom:_inset, right:_inset))
+        content.bounds = rect
+        content.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        
+        if _borderWidth != 0.0 {
+            content.layer.borderWidth = _borderWidth
+            content.layer.borderColor = view.tintColor.cgColor
+        }
+        
+        if isFullscreen && _blur {
+            // create a *inverse* round-rect path to fill the background but not under our effect view
+            if let shape = content.layer.sublayers?.first as? CAShapeLayer {
+                let path = CGMutablePath()
+                path.addRect(CGRect(x: -10_000_000, y: -10_000_000, width: 20_000_000, height: 20_000_000))
+                path.addRoundedRect(in:rect, cornerWidth:_inset, cornerHeight:_inset)
+                shape.path = path
+                shape.fillColor = _fullscreenColor.cgColor
+                shape.fillRule = .evenOdd
+            }
+        }
+    }
+    
     // MARK: buttons
     
-    @objc func buttonPress(_ sender:UIView?) {
-        guard let tag = sender?.tag, tag != -1 else {return}
+    private func tag2idx(_ tag:Int) -> Int? {
+        let idx = tag - 8675309
+        return actions.indices.contains(idx) ? idx : nil
+    }
+    private func idx2tag(_ idx:Int) -> Int {
+        return idx + 8675309
+    }
+
+    private func button(for action:UIAlertAction?) -> UIButton? {
+        if let action = action, let idx = actions.firstIndex(of:action) {
+            if let btn = stack.viewWithTag(idx2tag(idx)) as? UIButton {
+                return btn
+            }
+        }
+        return nil
+    }
+
+    private func action(for button:UIButton?) -> UIAlertAction? {
+        if let button = button, let idx = tag2idx(button.tag) {
+            return actions[idx]
+        }
+        return nil
+    }
+
+    @objc func buttonPress(_ sender:UIButton?) {
+        guard let action = action(for: sender) else {return}
         if autoDismiss {
             cancelAction = nil  // if we did the dismiss clear this
             self.presentingViewController?.dismiss(animated:true, completion: {
-                self.actions[tag].callActionHandler()
+                action.callActionHandler()
             })
         }
         else {
-            self.actions[tag].callActionHandler()
+            action.callActionHandler()
         }
     }
     @objc func buttonTap(_ sender:UITapGestureRecognizer) {
-        buttonPress(sender.view)
+        buttonPress(sender.view as? UIButton)
     }
     
     private func makeButton(_ action:UIAlertAction) -> UIView {
         guard let idx = actions.firstIndex(of:action) else {fatalError()}
         
         let btn = TVButton()
-        btn.tag = idx
-        btn.setTitle(action.title, for: .normal)
+        btn.tag = idx2tag(idx)
+        btn.setAttributedTitle(NSAttributedString(string:action.title ?? "", attributes:[.font:self.font]), for: .normal)
         btn.setTitleColor(UIColor.white, for: .normal)
         btn.setTitleColor(UIColor.gray, for: .disabled)
         
         let spacing = self.spacing
         btn.contentEdgeInsets = UIEdgeInsets(top:spacing, left:spacing*2, bottom:spacing, right:spacing*2)
         
-        btn.setGrowDelta(min(_insets.left, _insets.right) * 0.5, for: .focused)
+        btn.setGrowDelta(_inset * 0.5, for: .focused)
+        btn.setGrowDelta(_inset * 0.5, for: .selected)
 
-        #if os(iOS)
-            btn.font = self.font
-            btn.layer.cornerRadius = h/4
-            let h = font.lineHeight * 1.5
-            btn.addConstraint(NSLayoutConstraint(item:btn, attribute:.height, relatedBy:.equal, toItem:nil, attribute:.notAnAttribute, multiplier:1.0, constant:h))
-        #endif
+        let h = font.lineHeight * 1.5
+        btn.addConstraint(NSLayoutConstraint(item:btn, attribute:.height, relatedBy:.equal, toItem:nil, attribute:.notAnAttribute, multiplier:1.0, constant:h))
+        btn.layer.cornerRadius = h/4
 
         btn.addTarget(self, action: #selector(buttonPress(_:)), for: .primaryActionTriggered)
         
         if action.style == .destructive {
-            btn.backgroundColor = .systemRed
-            //btn.setTitleColor(.systemRed, for:.focused)
-            //btn.setTitleColor(.systemRed, for:.highlighted)
+            btn.backgroundColor = _destructiveButtonColor
+            btn.setBackgroundColor(_destructiveButtonColor.withAlphaComponent(1), for:.focused)
+            btn.setBackgroundColor(_destructiveButtonColor.withAlphaComponent(1), for:.highlighted)
+        }
+        else {
+            btn.backgroundColor = _defaultButtonColor
         }
         
         return btn
@@ -293,52 +387,29 @@ class TVAlertController: UIViewController, UIAlertControllerProtocol {
         }
     }
     
-    // MARK: layout and size
-    
-    private var isFullscreen: Bool {
-        return view.window?.bounds == view.bounds
-    }
-    
-    override var preferredContentSize: CGSize {
-        get {
-            var size = stack.systemLayoutSizeFitting(.zero)
-            size.width = min(size.width, _maxWidth)
-            if size != .zero {
-                size.width  += (_insets.left + _insets.right)
-                size.height += (_insets.top + _insets.bottom)
-            }
-            return size
-        }
-        set {
-            super.preferredContentSize = newValue
-        }
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        guard let content = view.subviews.first else {return}
-        
-        view.backgroundColor = isFullscreen ? _fullscreenColor : nil
-
-        let rect = CGRect(origin: .zero, size: self.preferredContentSize)
-        stack.frame = rect.inset(by: _insets)
-        content.bounds = rect
-        content.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
-     }
-    
     // MARK: focus
     
     override var preferredFocusEnvironments: [UIFocusEnvironment] {
         
         // if we have a preferredAction make that the first to get focus, but we gotta find it.
-        if let action = preferredAction, let idx = actions.firstIndex(of:action), idx != 0 {
-            if let view = stack.viewWithTag(idx) {
-                return [view]
-            }
+        if let button = button(for: preferredAction) {
+            return [button]
         }
         
         return super.preferredFocusEnvironments
     }
+    
+    #if os(iOS)
+    // if we dont have a FocusSystem then select the preferredAction
+    // TODO: detect focus sytem on iPad iOS 15+ ???
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let button = preferredFocusEnvironments.first as? TVButton {
+            button.isSelected = true
+        }
+    }
+    #endif
     
     // MARK: zoom in and zoom out
     
@@ -346,12 +417,12 @@ class TVAlertController: UIViewController, UIAlertControllerProtocol {
         super.viewWillAppear(animated)
         
         // get the content view, dont animate if we are in a popup
-        if let content = view.subviews.first, isFullscreen {
+        if let content = view.subviews.last, isFullscreen {
             let size = preferredContentSize
             let scale = min(1.0, min(view.bounds.size.width * 0.95 / size.width, view.bounds.size.height * 0.95 / size.height))
             
             content.transform = CGAffineTransform(scaleX:0.001, y:0.001)
-            UIView.animate(withDuration: 0.200) {
+            UIView.animate(withDuration: _animateDuration) {
                 content.transform = CGAffineTransform(scaleX:scale, y:scale)
             }
         }
@@ -361,8 +432,8 @@ class TVAlertController: UIViewController, UIAlertControllerProtocol {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
 
-        if let content = view.subviews.first, isFullscreen {
-            UIView.animate(withDuration: 0.200) {
+        if let content = view.subviews.last, isFullscreen {
+            UIView.animate(withDuration: _animateDuration) {
                 content.transform = CGAffineTransform(scaleX:0.001, y:0.001)
             }
         }
@@ -422,11 +493,14 @@ private class TVButton : UIButton {
     override func didMoveToWindow() {
         
         // these are the defaults if not set
-        _color[.normal]       = _color[.normal] ?? backgroundColor ?? .init(white:0.222, alpha:1)
+        _color[.normal]       = _color[.normal] ?? backgroundColor ?? .gray
         _color[.focused]      = _color[.focused] ?? tintColor
         _color[.highlighted]  = _color[.highlighted] ?? tintColor
-        
+        _color[.selected]     = _color[.selected] ?? tintColor
+
         _grow[.focused] = _grow[.focused] ?? 16.0
+        _grow[.highlighted] = _grow[.highlighted] ?? 0.0
+        _grow[.selected] = _grow[.selected] ?? 16.0
 
         if layer.cornerRadius == 0.0 {
             layer.cornerRadius = 12.0
@@ -440,7 +514,7 @@ private class TVButton : UIButton {
         if self.window != nil { update() }
     }
     func getBackgroundColor(for state: UIControl.State) -> UIColor? {
-        return  _color[state] ?? _color[state.subtracting(.focused)] ?? _color[state.subtracting(.highlighted)] ?? _color[.normal] ?? self.backgroundColor
+        return  _color[state] ?? _color[state.subtracting([.focused, .selected])] ?? _color[state.subtracting(.highlighted)] ?? _color[.normal] ?? self.backgroundColor
     }
 
     private var _grow = [UIControl.State:CGFloat]()
@@ -449,7 +523,7 @@ private class TVButton : UIButton {
         if self.window != nil { update() }
     }
     func getGrowDelta(for state: UIControl.State) -> CGFloat {
-        return _grow[state] ?? _grow[state.subtracting(.focused)] ?? _grow[state.subtracting(.highlighted)] ?? _grow[.normal] ?? 0.0
+        return _grow[state] ?? _grow[state.subtracting([.focused, .selected])] ?? _grow[state.subtracting(.highlighted)] ?? _grow[.normal] ?? 0.0
     }
 
     private func update() {
@@ -461,7 +535,14 @@ private class TVButton : UIButton {
     }
     override var isHighlighted: Bool {
         didSet {
-            UIView.animate(withDuration: 0.200) {
+            UIView.animate(withDuration: _animateDuration) {
+                self.update()
+            }
+        }
+    }
+    override var isSelected: Bool {
+        didSet {
+            UIView.animate(withDuration: _animateDuration) {
                 self.update()
             }
         }
