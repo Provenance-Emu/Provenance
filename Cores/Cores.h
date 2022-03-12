9c63cb4600abc9c265b3b2475e683534ec7f3595
Joseph Mattello <mail@joemattiello.com>
2022-03-12 06:08:49 -0500

    add framework targets for cores and expermnt cores
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

---
+++ Cores/Cores.h
@@ -0,0 +1,19 @@
+//
+//  Cores.h
+//  Cores
+//
+//  Created by Joseph Mattiello on 3/12/22.
+//  Copyright © 2022 Provenance Emu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Cores.
+FOUNDATION_EXPORT double CoresVersionNumber;
+
+//! Project version string for Cores.
+FOUNDATION_EXPORT const unsigned char CoresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cores/PublicHeader.h>
+
+
