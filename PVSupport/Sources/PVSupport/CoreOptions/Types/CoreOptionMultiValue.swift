e25f60e2f6c68c990451097c78bc9d24147562ff
Joseph Mattello <mail@joemattiello.com>
2022-03-23 00:10:21 -0400

    NOTICKET core options enum default fixed
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- PVSupport/Sources/PVSupport/CoreOptions/Types/CoreOptionMultiValue.swift
+++ PVSupport/Sources/PVSupport/CoreOptions/Types/CoreOptionMultiValue.swift
@@ -11,11 +11,12 @@ import Foundation
 public struct CoreOptionMultiValue {
     public let title: String
     public let description: String?
+    public let isDefault: Bool
 
     public static func values(fromArray a: [[String]]) -> [CoreOptionMultiValue] {
         return a.compactMap {
             if $0.count == 1 {
-                return .init(title: $0[0], description: nil)
+                return .init(title: $0[0], description: nil, isDefault: false)
             } else if $0.count >= 2 {
                 return .init(title: $0[0], description: $0[1])
             } else {
@@ -26,20 +27,22 @@ public struct CoreOptionMultiValue {
 
     public static func values(fromArray a: [String]) -> [CoreOptionMultiValue] {
         return a.map {
-            .init(title: $0, description: nil)
+            .init(title: $0, description: nil, isDefault: false)
         }
     }
 }
 
 extension CoreOptionMultiValue: Codable, Equatable, Hashable {}
 extension CoreOptionMultiValue {
-    public init(title: String, description: String) {
+    public init(title: String, description: String, isDefault: Bool = false) {
         self.title = title
         self.description = description
+        self.isDefault = isDefault
     }
 
     public init(_ title: String, _ description: String) {
         self.title = title
         self.description = description
+        self.isDefault = false
     }
 }
