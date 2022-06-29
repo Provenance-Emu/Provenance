b9e706436f22085b6c1339007b84dbb142d6a645
Joseph Mattello <mail@joemattiello.com>
2022-06-29 05:24:56 -0400

    make a libretro and it builds
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

---
+++ PVSupport/retro/retro.h
@@ -0,0 +1,13 @@
+//
+//  retro.h
+//  retro
+//
+//  Created by Joseph Mattiello on 6/29/22.
+//  Copyright © 2022 Provenance Emu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface retro : NSObject
+
+@end
---
+++ PVSupport/retro/retro.m
@@ -0,0 +1,13 @@
+//
+//  retro.m
+//  retro
+//
+//  Created by Joseph Mattiello on 6/29/22.
+//  Copyright © 2022 Provenance Emu. All rights reserved.
+//
+
+#import "retro.h"
+
+@implementation retro
+
+@end
