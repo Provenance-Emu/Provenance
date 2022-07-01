fdce03ee84fb84eb12f5b4867bb25967f8b1758c
Joseph Mattello <mail@joemattiello.com>
2022-06-30 22:22:43 -0400

    desume builds with libretro
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Cores/Desmume2015/PVDesmume2015Core/Core/PVDesmume2015Core.h
+++ Cores/Desmume2015/PVDesmume2015Core/Core/PVDesmume2015Core.h
@@ -10,11 +10,12 @@
 #import <PVSupport/PVSupport.h>
 #import <PVSupport/PVEmulatorCore.h>
 #import <PVSupport/PVSupport-Swift.h>
+#import <PVLibRetro/PVLibRetro.h>
 
 #define GET_CURRENT_AND_RETURN(...) __strong __typeof__(_current) current = _current; if(current == nil) return __VA_ARGS__;
 #define GET_CURRENT_OR_RETURN(...)  __strong __typeof__(_current) current = _current; if(current == nil) return __VA_ARGS__;
 
-@interface PVDesmume2015Core : PVEmulatorCore <PVDSSystemResponderClient>
+@interface PVDesmume2015Core : PVLibRetroCore <PVDSSystemResponderClient>
 {
 	uint8_t padData[4][PVDSButtonCount];
 	int8_t xAxis[4];
