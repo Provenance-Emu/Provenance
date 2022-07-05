12757d42c9fc71547dfac6b1cfb945f35b10b9cc
Joseph Mattiello <mail@joemattiello.com>
2022-07-05 00:28:35 -0400

    dosbox: some overwrides

--- Cores/DosBox/PBDosBoxCore/Core/PVDosBoxCore+Video.m
+++ Cores/DosBox/PBDosBoxCore/Core/PVDosBoxCore+Video.m
@@ -17,70 +17,70 @@
 
 # pragma mark - Methods
 
-- (void)videoInterrupt {
-        //dispatch_semaphore_signal(coreWaitToEndFrameSemaphore);
-
-        //dispatch_semaphore_wait(mupenWaitToBeginFrameSemaphore, DISPATCH_TIME_FOREVER);
-}
-
-- (void)swapBuffers {
-    [self.renderDelegate didRenderFrameOnAlternateThread];
-}
-
-- (void)executeFrameSkippingFrame:(BOOL)skip {
-
-	if (![self isEmulationPaused])
-	 {
-	 }
-        //dispatch_semaphore_signal(mupenWaitToBeginFrameSemaphore);
-
-        //dispatch_semaphore_wait(coreWaitToEndFrameSemaphore, DISPATCH_TIME_FOREVER);
-}
-
-- (void)executeFrame {
-    [self executeFrameSkippingFrame:NO];
-}
+//- (void)videoInterrupt {
+//        //dispatch_semaphore_signal(coreWaitToEndFrameSemaphore);
+//
+//        //dispatch_semaphore_wait(mupenWaitToBeginFrameSemaphore, DISPATCH_TIME_FOREVER);
+//}
+//
+//- (void)swapBuffers {
+//    [self.renderDelegate didRenderFrameOnAlternateThread];
+//}
+//
+//- (void)executeFrameSkippingFrame:(BOOL)skip {
+//
+//	if (![self isEmulationPaused])
+//	 {
+//	 }
+//        //dispatch_semaphore_signal(mupenWaitToBeginFrameSemaphore);
+//
+//        //dispatch_semaphore_wait(coreWaitToEndFrameSemaphore, DISPATCH_TIME_FOREVER);
+//}
+//
+//- (void)executeFrame {
+//    [self executeFrameSkippingFrame:NO];
+//}
 
 # pragma mark - Properties
 
 - (CGSize)bufferSize {
-    return CGSizeMake(1024, 512);
+    return CGSizeMake(640, 480);
 }
 
 - (CGRect)screenRect {
-    return CGRectMake(0, 0, self.videoWidth, self.videoHeight);
+    return CGRectMake(0, 0, 640, 480);
 }
 
 - (CGSize)aspectSize {
-    return CGSizeMake(self.videoWidth, self.videoHeight);
+    return CGSizeMake(640, 480);
 }
 
-- (BOOL)rendersToOpenGL {
-    return YES;
-}
+//- (BOOL)rendersToOpenGL {
+//    return YES;
+//}
+//
+//- (BOOL)isDoubleBuffered {
+//    return YES;
+//}
+//
+//- (const void *)videoBuffer {
+//    return NULL;
+//}
 
-- (BOOL)isDoubleBuffered {
-    return YES;
-}
-
-- (const void *)videoBuffer {
-    return NULL;
-}
-
-- (GLenum)pixelFormat {
-    return GL_RGBA;
-}
-
-- (GLenum)pixelType {
-    return GL_UNSIGNED_BYTE;
-}
-
-- (GLenum)internalPixelFormat {
-    return GL_RGBA;
-}
-
-- (GLenum)depthFormat {
-        // 0, GL_DEPTH_COMPONENT16, GL_DEPTH_COMPONENT24
-    return GL_DEPTH_COMPONENT24;
-}
+//- (GLenum)pixelFormat {
+//    return GL_RGBA;
+//}
+//
+//- (GLenum)pixelType {
+//    return GL_UNSIGNED_BYTE;
+//}
+//
+//- (GLenum)internalPixelFormat {
+//    return GL_RGBA;
+//}
+//
+//- (GLenum)depthFormat {
+//        // 0, GL_DEPTH_COMPONENT16, GL_DEPTH_COMPONENT24
+//    return GL_DEPTH_COMPONENT24;
+//}
 @end
