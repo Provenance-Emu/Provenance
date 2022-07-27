4ed3ea7a682252d1322ffc06782d68d595c47222
Joseph Mattello <mail@joemattiello.com>
2022-07-27 14:55:36 -0400

    retro: fbneo builds shockingly
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

---
+++ Cores/FinalBurnNeo/PVFBNeo/PVFBNeo.h
@@ -0,0 +1,18 @@
+//
+//  PVFBNeo.h
+//  PVFBNeo
+//
+//  Created by Joseph Mattiello on 6/15/22.
+//  Copyright © 2021 Provenance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PVFBNeo.
+FOUNDATION_EXPORT double PVFBNeoVersionNumber;
+
+//! Project version string for PVFBNeo.
+FOUNDATION_EXPORT const unsigned char PVFBNeoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PVFBNeo/PublicHeader.h>
+#import <PVFBNeo/PVFBNeoCore.h>
