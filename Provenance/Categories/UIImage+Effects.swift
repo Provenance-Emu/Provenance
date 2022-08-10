91d8ed82637ba6543be2339cb93b07bd33642cfb
Joseph Mattello <mail@joemattiello.com>
2022-08-10 01:52:23 -0400

    UIImage new effects
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

---
+++ Provenance/Categories/UIImage+Effects.swift
@@ -0,0 +1,28 @@
+//
+//  UIImage+Effects.swift
+//  Provenance
+//
+//  Created by Joseph Mattiello on 8/9/22.
+//  Copyright © 2022 Provenance Emu. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    var pixelated: UIImage? {
+        guard let currentCGImage = self.image.cgImage else { return nil }
+        let currentCIImage = CIImage(cgImage: currentCGImage)
+
+        guard let filter = CIFilter(name: "CIPixellate") else { return nil }
+        filter.setValue(currentCIImage, forKey: kCIInputImageKey)
+        filter.setValue(12, forKey: kCIInputScaleKey)
+        guard let outputImage = filter?.outputImage else { return nil}
+
+        let context = CIContext()
+
+        guard let cgimg = context.createCGImage(outputImage, from: outputImage.extent) else { return nil }
+        
+        let processedImage = UIImage(cgImage: cgimg)
+        return processedImage
+    }
+}
