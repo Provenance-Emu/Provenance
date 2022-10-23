fc1f2088d378ab3e439b7d614f88d19d1c5d8caa
Joseph Mattello <mail@joemattiello.com>
2022-10-23 03:38:27 -0400

    Add some demo code
    Signed-off-by: Joseph Mattiello <mail@joemattiello.com>
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>
    
    patreon: move to pvlib
    
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

---
+++ PVLibrary/PVLibrary/Patreon/PatreonAccount.swift
@@ -0,0 +1,55 @@
+//
+//  PatreonAccount.swift
+//  AltStore
+//
+//  Created by Riley Testut on 8/20/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+import CoreData
+
+@available(iOS 12.0, tvOS 12.0, *)
+extension PatreonAPI
+{
+    struct AccountResponse: Decodable
+    {
+        struct Data: Decodable
+        {
+            struct Attributes: Decodable
+            {
+                var first_name: String?
+                var full_name: String
+            }
+            
+            var id: String
+            var attributes: Attributes
+        }
+        
+        var data: Data
+        var included: [PatronResponse]?
+    }
+}
+
+@available(iOS 12.0, tvOS 12.0, *)
+public struct PatreonAccount: Codable
+{
+    public let identifier: String
+    
+	public let name: String
+	public let firstName: String?
+    
+	public let isPatron: Bool
+    
+    init(response: PatreonAPI.AccountResponse) {
+        self.identifier = response.data.id
+        self.name = response.data.attributes.full_name
+        self.firstName = response.data.attributes.first_name
+        
+        if let patronResponse = response.included?.first {
+            let patron = Patron(response: patronResponse)
+            self.isPatron = (patron.status == .active)
+        } else {
+            self.isPatron = false
+        }
+    }
+}
