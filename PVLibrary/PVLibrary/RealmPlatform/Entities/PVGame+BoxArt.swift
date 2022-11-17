f3eeeb82200cb5af2d56c9b9264e6cbfd68165a5
Joseph Mattello <mail@joemattiello.com>
2022-11-17 00:01:59 -0500

    BoxArt fix nil crash
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- PVLibrary/PVLibrary/RealmPlatform/Entities/PVGame+BoxArt.swift
+++ PVLibrary/PVLibrary/RealmPlatform/Entities/PVGame+BoxArt.swift
@@ -33,6 +33,7 @@ public enum PVGameBoxArtAspectRatio: CGFloat {
 
 public extension PVGame {
     var boxartAspectRatio: PVGameBoxArtAspectRatio {
+        guard let system = system else { return .square }
         switch system.enumValue {
         case .SGFX:
             return .sgx
@@ -68,7 +69,7 @@ public extension PVGame {
             default:
                 return .ggUSA
             }
-		case    .NES, .Sega32X, .Atari2600, .Atari5200, .Atari7800, .AtariJaguar, .AtariJaguarCD, .WonderSwan, .WonderSwanColor,
+		case .NES, .Sega32X, .Atari2600, .Atari5200, .Atari7800, .AtariJaguar, .AtariJaguarCD, .WonderSwan, .WonderSwanColor,
                 .MasterSystem, .SG1000, .FDS, .GameCube, .PS2, .Intellivision, .ColecoVision, ._3DO, .Odyssey2, .DOS, .MSX, .MSX2, .C64, .Wii, .PalmOS, .TIC80:
             return .tall
         case .GBA, .GBC, .Lynx, .NeoGeo, .NGP, .NGPC, .PCECD, .PCFX, .PokemonMini, .PSX, .SegaCD, .VirtualBoy, .DS, .Music, ._3DS, .MegaDuck:
