699b80c6934ee99564d2a0be25dc58f10f3cc5f8
Joseph Mattello <mail@joemattiello.com>
2022-12-14 05:13:47 -0500

    gcdweb: fix crash at close from dispatch priority
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Provenance/http/GCDWebServer/Core/GCDWebServer.m
+++ Provenance/http/GCDWebServer/Core/GCDWebServer.m
@@ -580,14 +580,14 @@ static inline NSString* _EncodeBase64(NSString* string) {
   _connectionClass = _GetOption(_options, GCDWebServerOption_ConnectionClass, [GCDWebServerConnection class]);
   _shouldAutomaticallyMapHEADToGET = [_GetOption(_options, GCDWebServerOption_AutomaticallyMapHEADToGET, @YES) boolValue];
   _disconnectDelay = [_GetOption(_options, GCDWebServerOption_ConnectedStateCoalescingInterval, @1.0) doubleValue];
-  _dispatchQueuePriority = [_GetOption(_options, GCDWebServerOption_DispatchQueuePriority, @(DISPATCH_QUEUE_PRIORITY_DEFAULT)) longValue];
+  _dispatchQueuePriority = [_GetOption(_options, GCDWebServerOption_DispatchQueuePriority, @(DISPATCH_QUEUE_PRIORITY_HIGH)) longValue];
 
   _source4 = [self _createDispatchSourceWithListeningSocket:listeningSocket4 isIPv6:NO];
   _source6 = [self _createDispatchSourceWithListeningSocket:listeningSocket6 isIPv6:YES];
   _port = port;
   _bindToLocalhost = bindToLocalhost;
 
-  NSString* bonjourName = _GetOption(_options, GCDWebServerOption_BonjourName, nil);
+  NSString* bonjourName = _GetOption(_options, GCDWebServerOption_BonjourName, _serverName);
   NSString* bonjourType = _GetOption(_options, GCDWebServerOption_BonjourType, @"_http._tcp");
   if (bonjourName) {
     _registrationService = CFNetServiceCreate(kCFAllocatorDefault, CFSTR("local."), (__bridge CFStringRef)bonjourType, (__bridge CFStringRef)(bonjourName.length ? bonjourName : _serverName), (SInt32)_port);
