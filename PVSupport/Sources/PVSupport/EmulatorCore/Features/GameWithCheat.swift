01cf77a812bfee68fe4ec2a9a10e9c8c6c060ae2
Richard <yuukifushimi@gmail.com>
2022-12-20 12:58:03 -0500

    Cheats: Support original setCheat API format

--- PVSupport/Sources/PVSupport/EmulatorCore/Features/GameWithCheat.swift
+++ PVSupport/Sources/PVSupport/EmulatorCore/Features/GameWithCheat.swift
@@ -4,6 +4,12 @@
 //
 import Foundation
 @objc public protocol GameWithCheat {
+    @objc(setCheatWithCode:type:enabled:)
+    optional func setCheat(
+        code: String,
+        type: String,
+        enabled: Bool
+    ) -> Bool
     @objc(setCheatWithCode:type:codeType:cheatIndex:enabled:)
     func setCheat(
         code: String,
