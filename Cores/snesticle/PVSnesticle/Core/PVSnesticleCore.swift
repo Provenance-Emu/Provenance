886dc3b486f6672d9841019bd63f998e5b2855f1
Joseph Mattello <mail@joemattiello.com>
2022-12-26 03:35:10 -0500

    snesticle: remove broken cheat swift code
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Cores/snesticle/PVSnesticle/Core/PVSnesticleCore.swift
+++ Cores/snesticle/PVSnesticle/Core/PVSnesticleCore.swift
@@ -6,24 +6,23 @@
 import PVSupport
 import Foundation
 
-extension PVSnesticleCore: GameWithCheat {
-    public func setCheat(
-        code: String,
-        type: String,
-        enabled: Bool
-    ) -> Bool
-    {
-        do {
-            try self.setCheat(code, setType: type, setEnabled: enabled)
-            return true
-        } catch let error {
-            NSLog("Error setCheat \(error)")
-            return false
-        }
-    }
-    
-    public func supportsCheatCode() -> Bool
-    {
-        return true
-    }
-}
+//extension PVSnesticleCore: GameWithCheat {
+//    public func setCheat(code: String, type: String, codeType: String, cheatIndex: UInt8, enabled: Bool) -> Bool {
+//        do{
+//            try self.setCheat(code, setType: type, setEnabled: enabled)
+//            return true
+//        } catch let error {
+//            NSLog("Error setCheat \(error)")
+//            return false
+//        }
+//        
+//        public func cheatCodeTypes() -> NSArray {
+//            ["Game Genie", "Pro Action Replay"]
+//        }
+//        
+//        public func supportsCheatCode() -> Bool
+//        {
+//            return true
+//        }
+//    }
+//}
