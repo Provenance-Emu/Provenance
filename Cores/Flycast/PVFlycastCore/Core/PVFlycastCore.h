f18a6706d140d1183bb57d7746d4dc578f072eca
Joseph Mattello <mail@joemattiello.com>
2023-01-16 13:56:17 -0500

    fbneo: start of make ios project
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Cores/Flycast/PVFlycastCore/Core/PVFlycastCore.h
+++ Cores/Flycast/PVFlycastCore/Core/PVFlycastCore.h
@@ -15,8 +15,8 @@
 #define GET_CURRENT_AND_RETURN(...) __strong __typeof__(_current) current = _current; if(current == nil) return __VA_ARGS__;
 #define GET_CURRENT_OR_RETURN(...)  __strong __typeof__(_current) current = _current; if(current == nil) return __VA_ARGS__;
 
-@interface PVFlycastCore : PVLibRetroGLESCore <PVDreamcastSystemResponderClient>
-{
+PVCORE
+@interface PVFlycastCore: PVLibRetroGLESCore <PVDreamcastSystemResponderClient> {
 	uint8_t padData[4][PVDreamcastButtonCount];
 	int8_t xAxis[4];
 	int8_t yAxis[4];
