36796790f9b706d8a356a31c0cf536f756b25a7f
Joseph Mattello <mail@joemattiello.com>
2023-01-16 19:30:53 -0500

    duckstation: untrack file
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Cores/DuckStation/cmake/src/scmversion/scmversion.cpp
+++
@@ -1,6 +0,0 @@
-// 9eea4d11d5765bcdc4c2b8e8c5cfa3c56f7c2323 pullrequests/rf2222222/retroarch_features2 1.5-alpha-2770-1714-g9eea4d11d-dirty 2023-01-16T17:26:26-05:00
-const char* g_scm_hash_str = "9eea4d11d5765bcdc4c2b8e8c5cfa3c56f7c2323";
-const char* g_scm_branch_str = "pullrequests/rf2222222/retroarch_features2";
-const char* g_scm_tag_str = "1.5-alpha-2770-1714-g9eea4d11d-dirty";
-const char* g_scm_date_str = "2023-01-16T17:26:26-05:00";
-
