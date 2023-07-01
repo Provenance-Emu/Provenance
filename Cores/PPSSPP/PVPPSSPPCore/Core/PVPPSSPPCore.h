58fe8b5911de8ff7fb3e4b065826260730e5ce43
Joseph Mattello <mail@joemattiello.com>
2023-07-01 03:58:05 -0400

    Fix PSP build order issue
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Cores/PPSSPP/PVPPSSPPCore/Core/PVPPSSPPCore.h
+++ Cores/PPSSPP/PVPPSSPPCore/Core/PVPPSSPPCore.h
@@ -5,14 +5,16 @@
 //  Created by Joseph Mattiello on 10/20/21.
 //  Copyright © 2021 Provenance. All rights reserved.
 //
+
 #import <Foundation/Foundation.h>
-#import <PVSupport/PVSupport.h>
-#import <PVSupport/PVEmulatorCore.h>
-#import <PVSupport/PVSupport-Swift.h>
 #import <UIKit/UIKit.h>
 #import <GLKit/GLKit.h>
 #import <Metal/Metal.h>
 #import <MetalKit/MetalKit.h>
+
+#import <PVSupport/PVSupport.h>
+#import <PVSupport/PVEmulatorCore.h>
+#import <PVSupport/PVSupport-Swift.h>
 
 #define MASKED_PSP_MEMORY 1
 #define GET_CURRENT_AND_RETURN(...) __strong __typeof__(_current) current = _current; if(current == nil) return __VA_ARGS__;
@@ -21,7 +23,7 @@
 @interface PVPPSSPPCore : PVEmulatorCore
 <PVPSPSystemResponderClient>
 {
-	uint8_t padData[4][PVDreamcastButtonCount];
+	uint8_t padData[4][PVPSPButtonCount];
 	int8_t xAxis[4];
 	int8_t yAxis[4];
 	int videoWidth;
