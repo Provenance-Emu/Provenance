dab78228ddcc7ed7a4cd409ef39d1d54247d5299
Dave Nicolson <david.nicolson@gmail.com>
2023-09-28 20:33:38 +0200

    Fix game pause alert

--- Provenance/Emulator/PVEmulatorVC/PVEmulatorViewController+PauseMenu.swift
+++ Provenance/Emulator/PVEmulatorVC/PVEmulatorViewController+PauseMenu.swift
@@ -19,13 +19,6 @@ extension PVEmulatorViewController {
 
         let actionSheet = UIAlertController(title: "Game Options", message: nil, preferredStyle: .actionSheet)
 
-        // only popup if sumoned from menuButton
-        if let menuButton = menuButton {
-            actionSheet.popoverPresentationController?.sourceView = menuButton
-            actionSheet.popoverPresentationController?.sourceRect = menuButton.bounds
-        } else {
-            return
-        }
 #if targetEnvironment(macCatalyst) || os(macOS)
         if let menuButton = menuButton, sender === menuButton {
             actionSheet.popoverPresentationController?.sourceView = menuButton
