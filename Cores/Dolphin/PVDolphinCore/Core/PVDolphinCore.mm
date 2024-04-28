ae69f6d31099219b8353cbace610678e8379b76e
Joseph Mattello <mail@joemattiello.com>
2024-04-27 22:08:24 -0400

    dolphin fix tvOS missing touch UI call
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- Cores/Dolphin/PVDolphinCore/Core/PVDolphinCore.mm
+++ Cores/Dolphin/PVDolphinCore/Core/PVDolphinCore.mm
@@ -624,7 +624,7 @@ void Host_Message(HostMessageID id)
 	if (Core::IsRunning())
 	  Core::QueueHostJob(&Core::Stop);
   } else if (id == HostMessageID::WMUserCreate)
-      NSLog(@"User Create Called\n", (int)id);
+      NSLog(@"User Create Called %i\n", (int)id);
 }
 
 void Host_UpdateTitle(const std::string& title)
