a8ce1297876cab36ff82bd4fbeced26569f6534d
Joseph Mattiello <git@joemattiello.com>
2024-10-16 19:05:51 -0400

    Fix mednafen missing gameboy
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Cores/Mednafen/Package.swift
+++ Cores/Mednafen/Package.swift
@@ -238,6 +238,7 @@ let package = Package(
             dependencies: [
                 /// Cores
                 "apple2",
+                "gb",
                 "gba",
                 "lynx",
                 "neogeopocket",
