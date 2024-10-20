30883c9eec8ac80003683a84e75288c9cf654828
Joseph Mattiello <git@joemattiello.com>
2024-10-20 00:22:48 -0400

    start to add an app rating framework
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

---
+++ Provenance/PVAppDelegate+AppRating.swift
@@ -0,0 +1,9 @@
+//
+//  PVAppDelegate+AppRating.swift
+//  Provenance
+//
+//  Created by Joseph Mattiello on 10/19/24.
+//  Copyright © 2024 Provenance Emu. All rights reserved.
+//
+
+import Foundation
