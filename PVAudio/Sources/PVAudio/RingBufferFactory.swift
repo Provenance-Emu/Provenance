f54e3ab361c4a8ca486be2f4a3838c5b539c5e26
Joseph Mattiello <git@joemattiello.com>
2024-11-02 23:35:38 -0400

    ring buffer factory logging
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- PVAudio/Sources/PVAudio/RingBufferFactory.swift
+++ PVAudio/Sources/PVAudio/RingBufferFactory.swift
@@ -10,6 +10,7 @@ import Foundation
 import PVRingBuffer
 import OERingBuffer
 import AppleRingBuffer
+import PVLogging
 
 @objc
 public enum RingBufferType: Int {
@@ -21,12 +22,25 @@ public enum RingBufferType: Int {
 public final class RingBufferFactory: NSObject {
     @objc
     public static func make(type: RingBufferType = .openEMU, withLength length: Int) -> RingBufferProtocol? {
+        // Log the requested buffer creation
+        DLOG("Creating \(type) ring buffer with length: \(length)")
+
+        let buffer: RingBufferProtocol?
         switch type {
             case .openEMU:
-            return OERingBuffer(withLength: length)
+                buffer = OERingBuffer(withLength: length)
             case .provenance:
-            return PVRingBuffer(withLength: length)
+                buffer = PVRingBuffer(withLength: length)
         }
+
+        // Log the result
+        if buffer == nil {
+            ELOG("Failed to create \(type) ring buffer")
+        } else {
+            DLOG("Successfully created \(type) ring buffer")
+        }
+
+        return buffer
     }
 }
 
