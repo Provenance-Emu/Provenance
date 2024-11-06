52edd323b700e3ff45bd1ebc814b6a2bbc32e6db
Joseph Mattiello <git@joemattiello.com>
2024-11-06 04:25:19 -0500

    fix runtime issues with audio engine
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- PVCoreAudio/Sources/PVCoreAudio/GameAudioEngine2.swift
+++ PVCoreAudio/Sources/PVCoreAudio/GameAudioEngine2.swift
@@ -1,27 +1,3 @@
-// Copyright (c) 2022, OpenEmu Team
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the OpenEmu Team nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY OpenEmu Team ''AS IS'' AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL OpenEmu Team BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 import Foundation
 import AVFoundation
 import PVLogging
@@ -258,10 +234,12 @@ final public class GameAudioEngine2: AudioEngineProtocol {
             let bytesRequested = Int(frameCount * bytesPerFrame)
             let bytesCopied = read(inputData.pointee.mBuffers.mData!, bytesRequested)
 
-            DLOG("Requested: \(bytesRequested) bytes, Copied: \(bytesCopied) bytes")
-
-            inputData.pointee.mBuffers.mDataByteSize = UInt32(bytesCopied)
-            inputData.pointee.mBuffers.mNumberChannels = 2  // Always stereo for Genesis
+            /// Only update if we actually got data
+            if bytesCopied > 0 {
+                inputData.pointee.mBuffers.mDataByteSize = UInt32(bytesCopied)
+                inputData.pointee.mBuffers.mNumberChannels = sd.mChannelsPerFrame
+                DLOG("Requested: \(bytesRequested) bytes, Copied: \(bytesCopied) bytes")
+            }
 
             return noErr
         }
