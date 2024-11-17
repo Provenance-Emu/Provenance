f9f50905fac87cd80a418a4db1d8bd033e07a025
Joseph Mattiello <git@joemattiello.com>
2024-11-17 18:20:27 -0500

    fix bg color swift compiler issue
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- PVUI/Sources/PVSwiftUI/Imports/ImportTaskRowView.swift
+++ PVUI/Sources/PVSwiftUI/Imports/ImportTaskRowView.swift
@@ -12,7 +12,7 @@ import Perception
 
 func iconNameForStatus(_ status: ImportStatus) -> String {
     switch status {
-        
+
     case .queued:
         return "line.3.horizontal.circle"
     case .processing:
@@ -33,11 +33,11 @@ struct ImportTaskRowView: View {
     let item: ImportQueueItem
     @State private var isNavigatingToSystemSelection = false
     @ObservedObject private var themeManager = ThemeManager.shared
-    
+
     var bgColor: Color {
         themeManager.currentPalette.settingsCellBackground?.swiftUIColor ?? themeManager.currentPalette.menuBackground.swiftUIColor
     }
-    
+
     var body: some View {
         WithPerceptionTracking {
             HStack {
@@ -54,11 +54,11 @@ struct ImportTaskRowView: View {
                             .font(.subheadline)
                             .foregroundColor(themeManager.currentPalette.gameLibraryText.swiftUIColor)
                     }
-                    
+
                 }
-                
+
                 Spacer()
-                
+
                 VStack(alignment: .trailing) {
                     if item.status == .processing {
                         ProgressView().progressViewStyle(.circular).frame(width: 40, height: 40, alignment: .center)
@@ -66,17 +66,21 @@ struct ImportTaskRowView: View {
                         Image(systemName: iconNameForStatus(item.status))
                             .foregroundColor(item.status.color)
                     }
-                    
+
                     if (item.childQueueItems.count > 0) {
                         Text("+\(item.childQueueItems.count) files")
                             .font(.subheadline)
                             .foregroundColor(themeManager.currentPalette.gameLibraryText.swiftUIColor)
                     }
                 }
-                
+
             }
             .padding()
-            .background(bgColor)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(bgColor)
+                    .shadow(radius: 2)
+            )
             .onTapGesture {
                 if item.status == .conflict {
                     isNavigatingToSystemSelection = true
