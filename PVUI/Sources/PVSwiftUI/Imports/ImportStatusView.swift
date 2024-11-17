d185e37683a7d07d721b9ce727747cbdd49a0d8b
Joseph Mattiello <git@joemattiello.com>
2024-11-17 13:10:09 -0500

    Fix perecption tracking runtime warnings
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- PVUI/Sources/PVSwiftUI/Imports/ImportStatusView.swift
+++ PVUI/Sources/PVSwiftUI/Imports/ImportStatusView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 import PVLibrary
 import PVThemes
+import Perception
 
 public protocol ImportStatusDelegate : AnyObject {
     func dismissAction()
@@ -17,24 +18,24 @@ public protocol ImportStatusDelegate : AnyObject {
 }
 
 func iconNameForFileType(_ type: FileType) -> String {
-
+    
     switch type {
-        case .bios:
-            return "bios_filled"
-        case .artwork:
-            return "image_icon_256"
-        case .game:
-            return "file_icon_256"
-        case .cdRom:
-            return "cd_icon_256"
-        case .unknown:
-            return "questionMark"
+    case .bios:
+        return "bios_filled"
+    case .artwork:
+        return "image_icon_256"
+    case .game:
+        return "file_icon_256"
+    case .cdRom:
+        return "cd_icon_256"
+    case .unknown:
+        return "questionMark"
     }
 }
 
 func iconNameForStatus(_ status: ImportStatus) -> String {
     switch status {
-
+        
     case .queued:
         return "line.3.horizontal.circle"
     case .processing:
@@ -55,56 +56,58 @@ struct ImportTaskRowView: View {
     let item: ImportQueueItem
     @State private var isNavigatingToSystemSelection = false
     @ObservedObject private var themeManager = ThemeManager.shared
-
+    
     var body: some View {
-        HStack {
-            //TODO: add icon for fileType
-            VStack(alignment: .leading) {
-                Text(item.url.lastPathComponent)
-                    .font(.headline)
-                if item.fileType == .bios {
-                    Text("BIOS")
-                        .font(.subheadline)
-                        .foregroundColor(themeManager.currentPalette.gameLibraryText.swiftUIColor)
-                } else if !item.systems.isEmpty {
-                    Text("\(item.systems.count) systems")
-                        .font(.subheadline)
-                        .foregroundColor(themeManager.currentPalette.gameLibraryText.swiftUIColor)
+        WithPerceptionTracking {
+            HStack {
+                //TODO: add icon for fileType
+                VStack(alignment: .leading) {
+                    Text(item.url.lastPathComponent)
+                        .font(.headline)
+                    if item.fileType == .bios {
+                        Text("BIOS")
+                            .font(.subheadline)
+                            .foregroundColor(themeManager.currentPalette.gameLibraryText.swiftUIColor)
+                    } else if !item.systems.isEmpty {
+                        Text("\(item.systems.count) systems")
+                            .font(.subheadline)
+                            .foregroundColor(themeManager.currentPalette.gameLibraryText.swiftUIColor)
+                    }
+                    
                 }
-
-            }
-
-            Spacer()
-
-            VStack(alignment: .trailing) {
-                if item.status == .processing {
-                    ProgressView().progressViewStyle(.circular).frame(width: 40, height: 40, alignment: .center)
-                } else {
-                    Image(systemName: iconNameForStatus(item.status))
-                        .foregroundColor(item.status.color)
-                }
-
-                if (item.childQueueItems.count > 0) {
-                    Text("+\(item.childQueueItems.count) files")
-                        .font(.subheadline)
-                        .foregroundColor(themeManager.currentPalette.gameLibraryText.swiftUIColor)
-                }
-            }
-
-        }
-        .padding()
-        .background(Color.white)
-        .onTapGesture {
-                    if item.status == .conflict {
-                        isNavigatingToSystemSelection = true
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    if item.status == .processing {
+                        ProgressView().progressViewStyle(.circular).frame(width: 40, height: 40, alignment: .center)
+                    } else {
+                        Image(systemName: iconNameForStatus(item.status))
+                            .foregroundColor(item.status.color)
+                    }
+                    
+                    if (item.childQueueItems.count > 0) {
+                        Text("+\(item.childQueueItems.count) files")
+                            .font(.subheadline)
+                            .foregroundColor(themeManager.currentPalette.gameLibraryText.swiftUIColor)
                     }
                 }
-                .background(
-                    NavigationLink(destination: SystemSelectionView(item: item), isActive: $isNavigatingToSystemSelection) {
-                        EmptyView()
-                    }
+                
+            }
+            .padding()
+            .background(Color.white)
+            .onTapGesture {
+                if item.status == .conflict {
+                    isNavigatingToSystemSelection = true
+                }
+            }
+            .background(
+                NavigationLink(destination: SystemSelectionView(item: item), isActive: $isNavigatingToSystemSelection) {
+                    EmptyView()
+                }
                     .hidden()
-                )
+            )
+        }
     }
 }
 
@@ -112,12 +115,13 @@ struct ImportStatusView: View {
     @ObservedObject var updatesController: PVGameLibraryUpdatesController
     var gameImporter:GameImporter
     weak var delegate:ImportStatusDelegate!
-
+    
     private func deleteItems(at offsets: IndexSet) {
         gameImporter.removeImports(at: offsets)
     }
-
+    
     var body: some View {
+        WithPerceptionTracking {
             NavigationView {
                 List {
                     if gameImporter.importQueue.isEmpty {
@@ -153,8 +157,9 @@ struct ImportStatusView: View {
             .presentationDetents([.medium, .large])
             .presentationDragIndicator(.visible)
         }
+    }
 }
 
-#Preview {
-
-}
+//#Preview {
+//    
+//}
