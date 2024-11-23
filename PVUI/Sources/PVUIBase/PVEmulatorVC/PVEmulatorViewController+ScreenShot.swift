9b01bb2010e61589927ef6051086630543264a19
Joseph Mattiello <git@joemattiello.com>
2024-11-22 22:37:34 -0500

    screenshots, gles/metal square
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- PVUI/Sources/PVUIBase/PVEmulatorVC/PVEmulatorViewController+ScreenShot.swift
+++ PVUI/Sources/PVUIBase/PVEmulatorVC/PVEmulatorViewController+ScreenShot.swift
@@ -13,11 +13,14 @@ extension PVEmulatorViewController {
     func captureScreenshot() -> UIImage? {
         fpsLabel.alpha = 0.0
         if (core.skipLayout && core.touchViewController != nil) {
-            let width: CGFloat? = UIScreen.main.bounds.size.width
-            let height: CGFloat? = UIScreen.main.bounds.size.height
-            let size = CGSize(width: width ?? 0.0, height: height ?? 0.0)
+            let width: CGFloat = UIScreen.main.bounds.size.width ?? 0.0
+            let height: CGFloat = UIScreen.main.bounds.size.height ?? 0.0
+            /// square size becuase we don't really know the size often
+            /// TODO: Find a better way to do this
+            let squareSize = min(width, height)
+            let size = CGSize(width: squareSize, height: squareSize)
             UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.main.scale)
-            let rec = CGRect(x: 0, y: 0, width: width ?? 0.0, height: height ?? 0.0)
+            let rec = CGRect(x: 0, y: 0, width: squareSize, height: squareSize)
             core.touchViewController?.view.drawHierarchy(in: rec, afterScreenUpdates: true)
         } else {
             let width: CGFloat? = gpuViewController.view.frame.size.width > 0 ? gpuViewController.view.frame.size.width : UIScreen.main.bounds.width
