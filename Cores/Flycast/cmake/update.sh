f1b3afd6babc604e980c7137f5c4e451db978a9f
Joseph Mattiello <git@joemattiello.com>
2024-11-26 18:51:59 -0500

    update the flycast update script
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Cores/Flycast/cmake/update.sh
+++ Cores/Flycast/cmake/update.sh
@@ -1,13 +1,20 @@
 #/usr/bin/env bash
 export Vulkan_LIBRARY=1.3.250 # Version of Vulkan SDK
-export VULKAN_SDK="./MoltenVK" # Path to Vulkan SDK
-export Vulkan_INCLUDE_DIR="./MoltenVK/include"
-export FRAMEWORK_VULKAN_LIBS="./MoltenVK/dylib/iOS/libMoltenVK.dylib"
-export Vulkan_LIBRARY="./MoltenVK"
+export VULKAN_SDK="../../../MoltenVK/MoltenVK" # Path to Vulkan SDK
+export Vulkan_INCLUDE_DIR="../../../MoltenVK/MoltenVK/include"
+export FRAMEWORK_VULKAN_LIBS="../../../MoltenVK/MoltenVK/dynamic/MoltenVK.xcframework"
+export Vulkan_LIBRARY="../../../MoltenVK/MoltenVK"
+export RELATIVE_PATH=1
+export CMAKE_USE_RELATIVE_PATHS=ON
+export CMAKE_SKIP_RPATH=ON
+export VERBOSE=1
+
+# Remove previous CMake configuration
+rm -rf CMakeCache.txt CMakeFiles/
 
 # This sets mobile to true...
 echo "VULKAN_SDK="${VULKAN_SDK}
-cmake ../flycast -G Xcode \
+cmake -B . -S ../flycast -G Xcode \
 -DCMAKE_SYSTEM_NAME=iOS \
 -DUSE_GLES2=ON \
 -DLIBRETRO=ON \
@@ -20,6 +27,10 @@ cmake ../flycast -G Xcode \
 -D_OPENMP=ON \
 -D__APPLE__=ON \
 -DFRAMEWORK_VULKAN_LIBS=${FRAMEWORK_VULKAN_LIBS} \
+-DCMAKE_USE_RELATIVE_PATHS=ON \
+-DCMAKE_SKIP_RPATH=ON \
+-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
+-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=OFF \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_C_FLAGS_RELEASE="-Ofast -DNDEBUG" \
 -DCMAKE_CXX_FLAGS_RELEASE="-Ofast -DNDEBUG" \
@@ -30,6 +41,6 @@ cmake ../flycast -G Xcode \
 -DCMAKE_XCODE_ATTRIBUTE_PROVISIONING_PROFILE_SPECIFIER="iOS Team Provisioning Profile: *"
 
 # Path adjustements
-#python3 xcode_absolute_path_to_relative.py
-#find . -name "*.make" -exec rm {} \;
-#find . -name "*.cmake" -exec rm {} \;
+python3 xcode_absolute_path_to_relative.py
+find . -name "*.make" -exec rm {} \;
+find . -name "*.cmake" -exec rm {} \;
