f3bcfc3ce0a52aa4b60a9a03034352e6148be77b
Joseph Mattiello <git@joemattiello.com>
2024-12-15 01:29:47 -0500

    fix openvgdb mapping from struct
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- PVLibrary/Sources/PVLibrary/Extensions/ROMMetadata+PVSystem.swift
+++ PVLibrary/Sources/PVLibrary/Extensions/ROMMetadata+PVSystem.swift
@@ -4,6 +4,6 @@ import PVLookupTypes
 public extension ROMMetadata {
     /// The corresponding PVSystem for this ROM metadata
     var system: PVSystem? {
-        return PVEmulatorConfiguration.system(forDatabaseID: systemID)
+        return PVEmulatorConfiguration.system(forDatabaseID: systemID.openVGDBID)
     }
 }
