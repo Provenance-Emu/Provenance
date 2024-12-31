545c7590c56f7c7225e95175256a0fa62e39d27a
Joseph Mattiello <git@joemattiello.com>
2024-12-31 13:27:10 -0500

    fMSX fix build, add to lite
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

---
+++ Cores/fmsx/PVfMSXCore/CorePlist-Generated.swift
@@ -0,0 +1,39 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+#if canImport(PVCoreBridge)
+@_exported import PVCoreBridge
+@_exported import PVPlists
+#endif
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Plist Files
+
+// swiftlint:disable identifier_name line_length number_separator type_body_length
+public enum CorePlist {
+  public static let pvCoreIdentifier: String = "com.provenance.core.fMSX"
+  public static let pvPrincipleClass: String = "PVfMSX.PVfMSXEmuCore"
+  public static let pvProjectName: String = "fMSX"
+  public static let pvProjectURL: String = "http://fms.komkon.org/fMSX/"
+  public static let pvProjectVersion: String = "0"
+  public static let pvSupportedSystems: [String] = ["com.provenance.msx", "com.provenance.msx2"]
+
+  #if canImport(PVCoreBridge)
+    public static var corePlist: EmulatorCoreInfoPlist {
+        .init(
+            identifier: CorePlist.pvCoreIdentifier,
+            principleClass: CorePlist.pvPrincipleClass,
+            supportedSystems: CorePlist.pvSupportedSystems,
+            projectName: CorePlist.pvProjectName,
+            projectURL: CorePlist.pvProjectURL,
+            projectVersion: CorePlist.pvProjectVersion)
+    }
+
+    public var corePlist: EmulatorCoreInfoPlist { Self.corePlist }
+  #endif
+}
+// swiftlint:enable identifier_name line_length number_separator type_body_length
