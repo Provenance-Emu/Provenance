f8a289ac3a61c9181d4f2cfa7cf03549be0a4ef0
Joseph Mattiello <git@joemattiello.com>
2025-01-06 11:56:27 -0500

    duckstation add scmversion.cpp to repo
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

---
+++ Cores/DuckStation/cmake/src/scmversion/scmversion.cpp
@@ -0,0 +1,6 @@
+// e7738210551211baadf4991ff5724f6563565bcc feature/new-cores/libretrogles 1.5-alpha-2770-3252-ge77382105-dirty 2025-01-02T19:32:12-05:00
+const char* g_scm_hash_str = "e7738210551211baadf4991ff5724f6563565bcc";
+const char* g_scm_branch_str = "feature/new-cores/libretrogles";
+const char* g_scm_tag_str = "1.5-alpha-2770-3252-ge77382105-dirty";
+const char* g_scm_date_str = "2025-01-02T19:32:12-05:00";
+
