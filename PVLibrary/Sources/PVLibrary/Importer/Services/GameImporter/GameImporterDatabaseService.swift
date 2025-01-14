c6224c95edb40f7537b04bf6c5229c5b9b4a43ac
pqsk <3964831+pabloarista@users.noreply.github.com>
2025-01-13 23:42:55 -0500

    added todo to test without .icloud relative

--- PVLibrary/Sources/PVLibrary/Importer/Services/GameImporter/GameImporterDatabaseService.swift
+++ PVLibrary/Sources/PVLibrary/Importer/Services/GameImporter/GameImporterDatabaseService.swift
@@ -216,7 +216,7 @@ class GameImporterDatabaseService : GameImporterDatabaseServicing {
         }
         if PVMediaCache.fileExists(forKey: url) {
             if let localURL = PVMediaCache.filePath(forKey: url) {
-                let file = PVImageFile(withURL: localURL, relativeRoot: .iCloud)
+                let file = PVImageFile(withURL: localURL, relativeRoot: .iCloud)//TODO: test without .iCloud
                 game.originalArtworkFile = file
                 return game
             }
@@ -240,7 +240,7 @@ class GameImporterDatabaseService : GameImporterDatabaseServicing {
             if let artwork = NSImage(data: data) {
                 do {
                     let localURL = try PVMediaCache.writeImage(toDisk: artwork, withKey: url)
-                    let file = PVImageFile(withURL: localURL, relativeRoot: .iCloud)
+                    let file = PVImageFile(withURL: localURL, relativeRoot: .iCloud)//TODO: test without .iCloud
                     game.originalArtworkFile = file
                 } catch { ELOG("\(error.localizedDescription)") }
             }
@@ -248,7 +248,7 @@ class GameImporterDatabaseService : GameImporterDatabaseServicing {
             if let artwork = UIImage(data: data) {
                 do {
                     let localURL = try PVMediaCache.writeImage(toDisk: artwork, withKey: url)
-                    let file = PVImageFile(withURL: localURL, relativeRoot: .iCloud)
+                    let file = PVImageFile(withURL: localURL, relativeRoot: .iCloud)//TODO: test without .iCloud
                     game.originalArtworkFile = file
                 } catch { ELOG("\(error.localizedDescription)") }
             }
