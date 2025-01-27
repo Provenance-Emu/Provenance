07f1b5840838f6aefa0fbfdef590f0446703f6fe
pqsk <3964831+pabloarista@users.noreply.github.com>
2025-01-26 20:57:11 -0500

    removed field

--- PVLibrary/Sources/PVLibrary/Database/Notifications.swift
+++ PVLibrary/Sources/PVLibrary/Database/Notifications.swift
@@ -10,7 +10,6 @@ import Foundation
 public extension Notification.Name {
     static let DatabaseMigrationStarted = Notification.Name("DatabaseMigrarionStarted")
     static let DatabaseMigrationFinished = Notification.Name("DatabaseMigrarionFinished")
-    static let NewCloudFilesAvailable = Notification.Name("NewCloudFilesAvailable")
     static let RomDatabaseInitialized = Notification.Name("RomDatabaseInitialized")
     static let RomsFinishedImporting = Notification.Name("RomsFinishedImporting")
 }
