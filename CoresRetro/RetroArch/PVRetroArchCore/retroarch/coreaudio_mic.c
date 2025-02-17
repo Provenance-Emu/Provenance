d768ffb7576ca4d1cd0d0bb16915738fd20295e6
Joseph Mattiello <git@joemattiello.com>
2025-02-17 17:26:43 -0500

    RetroArch Add coreaudio_mic
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

---
+++ CoresRetro/RetroArch/PVRetroArchCore/retroarch/coreaudio_mic.c
@@ -0,0 +1,331 @@
+/*  RetroArch - A frontend for libretro.
+ *  Copyright (C) 2025 - Joseph Mattiello
+ *
+ *  RetroArch is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ */
+
+#import <AudioToolbox/AudioToolbox.h>
+#include "audio/microphone_driver.h"
+#include "queues/fifo_queue.h"
+#include "verbosity.h"
+#include <memory.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+#include "audio/audio_driver.h"
+#include "../../verbosity.h"
+
+typedef struct coreaudio_microphone_handle
+{
+    AudioComponentInstance audio_unit; /// CoreAudio audio unit
+    fifo_buffer_t *sample_buffer;      /// Queue for storing samples
+    AudioStreamBasicDescription format; /// Audio format
+    bool is_running;                   /// Whether audio unit is running
+    bool use_float;                    /// Whether using float samples
+} coreaudio_microphone_handle_t;
+
+typedef struct coreaudio_microphone
+{
+    bool nonblock; /// Non-blocking mode flag
+} coreaudio_microphone_t;
+
+/// Callback for receiving audio samples
+static OSStatus coreaudio_input_callback(
+    void *inRefCon,
+    AudioUnitRenderActionFlags *ioActionFlags,
+    const AudioTimeStamp *inTimeStamp,
+    UInt32 inBusNumber,
+    UInt32 inNumberFrames,
+    AudioBufferList *ioData)
+{
+    coreaudio_microphone_handle_t *microphone = (coreaudio_microphone_handle_t*)inRefCon;
+    AudioBufferList bufferList;
+    OSStatus status;
+
+    bufferList.mNumberBuffers = 1;
+    bufferList.mBuffers[0].mDataByteSize = inNumberFrames * microphone->format.mBytesPerFrame;
+    bufferList.mBuffers[0].mData = malloc(bufferList.mBuffers[0].mDataByteSize);
+
+    status = AudioUnitRender(microphone->audio_unit,
+                           ioActionFlags,
+                           inTimeStamp,
+                           inBusNumber,
+                           inNumberFrames,
+                           &bufferList);
+
+    if (status == noErr) {
+        fifo_write(microphone->sample_buffer,
+                  bufferList.mBuffers[0].mData,
+                  bufferList.mBuffers[0].mDataByteSize);
+    } else {
+        RARCH_ERR("[CoreAudio]: Failed to write samples to buffer: %d\n", status);
+    }
+
+    free(bufferList.mBuffers[0].mData);
+    return status;
+}
+
+/// Initialize CoreAudio microphone driver
+static void *coreaudio_microphone_init(void)
+{
+    coreaudio_microphone_t *coreaudio = (coreaudio_microphone_t*)calloc(1, sizeof(*coreaudio));
+    if (!coreaudio) {
+        RARCH_ERR("[CoreAudio]: Failed to allocate microphone driver\n");
+        return NULL;
+    }
+
+    return coreaudio;
+}
+
+/// Free CoreAudio microphone driver
+static void coreaudio_microphone_free(void *driver_context)
+{
+    coreaudio_microphone_t *coreaudio = (coreaudio_microphone_t*)driver_context;
+    if (coreaudio)
+        free(coreaudio);
+}
+
+/// Read samples from microphone
+static int coreaudio_microphone_read(void *driver_context,
+                                    void *microphone_context,
+                                    void *buf,
+                                    size_t size)
+{
+    coreaudio_microphone_t *coreaudio = (coreaudio_microphone_t*)driver_context;
+    coreaudio_microphone_handle_t *microphone = (coreaudio_microphone_handle_t*)microphone_context;
+    size_t avail, read_amt;
+
+    if (!coreaudio || !microphone || !buf) {
+        RARCH_ERR("[CoreAudio]: Invalid parameters in read\n");
+        return -1;
+    }
+
+    avail = FIFO_READ_AVAIL(microphone->sample_buffer);
+    read_amt = MIN(avail, size);
+
+    if (read_amt > 0) {
+        fifo_read(microphone->sample_buffer, buf, read_amt);
+        RARCH_LOG("[CoreAudio]: Read %zu bytes from microphone\n", read_amt);
+    }
+
+    return (int)read_amt;
+}
+
+/// Set non-blocking state
+static void coreaudio_microphone_set_nonblock_state(void *driver_context, bool state)
+{
+    coreaudio_microphone_t *coreaudio = (coreaudio_microphone_t*)driver_context;
+    if (coreaudio)
+        coreaudio->nonblock = state;
+}
+
+/// Open microphone device
+static void *coreaudio_microphone_open_mic(void *driver_context,
+                                         const char *device,
+                                         unsigned rate,
+                                         unsigned latency,
+                                         unsigned *new_rate)
+{
+    coreaudio_microphone_handle_t *microphone = (coreaudio_microphone_handle_t*)calloc(1, sizeof(*microphone));
+    if (!microphone)
+        return NULL;
+
+    // Initialize audio unit
+    AudioComponentDescription desc = {
+        .componentType = kAudioUnitType_Output,
+#if TARGET_OS_IPHONE
+        .componentSubType = kAudioUnitSubType_RemoteIO,
+#else
+        .componentSubType = kAudioUnitSubType_HALOutput,
+#endif
+        .componentManufacturer = kAudioUnitManufacturer_Apple,
+        .componentFlags = 0,
+        .componentFlagsMask = 0
+    };
+
+    AudioComponent comp = AudioComponentFindNext(NULL, &desc);
+    OSStatus status = AudioComponentInstanceNew(comp, &microphone->audio_unit);
+    if (status != noErr) {
+        RARCH_ERR("[CoreAudio]: Failed to create audio unit\n");
+        goto error;
+    }
+
+    // Enable input
+    UInt32 flag = 1;
+    status = AudioUnitSetProperty(microphone->audio_unit,
+                                 kAudioOutputUnitProperty_EnableIO,
+                                 kAudioUnitScope_Input,
+                                 1, // Input bus
+                                 &flag,
+                                 sizeof(flag));
+    if (status != noErr) {
+        RARCH_ERR("[CoreAudio]: Failed to enable input\n");
+        goto error;
+    }
+
+    // Set format
+    microphone->format.mSampleRate = rate;
+    microphone->format.mFormatID = kAudioFormatLinearPCM;
+    microphone->format.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
+    microphone->format.mFramesPerPacket = 1;
+    microphone->format.mChannelsPerFrame = 1;
+    microphone->format.mBitsPerChannel = 16;
+    microphone->format.mBytesPerFrame = microphone->format.mChannelsPerFrame * microphone->format.mBitsPerChannel / 8;
+    microphone->format.mBytesPerPacket = microphone->format.mBytesPerFrame * microphone->format.mFramesPerPacket;
+
+    status = AudioUnitSetProperty(microphone->audio_unit,
+                                 kAudioUnitProperty_StreamFormat,
+                                 kAudioUnitScope_Output,
+                                 1, // Input bus
+                                 &microphone->format,
+                                 sizeof(microphone->format));
+    if (status != noErr) {
+        RARCH_ERR("[CoreAudio]: Failed to set format\n");
+        goto error;
+    }
+
+    // Set callback
+    AURenderCallbackStruct callback = { coreaudio_input_callback, microphone };
+    status = AudioUnitSetProperty(microphone->audio_unit,
+                                 kAudioOutputUnitProperty_SetInputCallback,
+                                 kAudioUnitScope_Global,
+                                 1, // Input bus
+                                 &callback,
+                                 sizeof(callback));
+    if (status != noErr) {
+        RARCH_ERR("[CoreAudio]: Failed to set callback\n");
+        goto error;
+    }
+
+    // Initialize audio unit
+    status = AudioUnitInitialize(microphone->audio_unit);
+    if (status != noErr) {
+        RARCH_ERR("[CoreAudio]: Failed to initialize audio unit\n");
+        goto error;
+    }
+
+    // Create sample buffer
+    microphone->sample_buffer = fifo_new(latency * rate * microphone->format.mBytesPerFrame / 1000);
+    if (!microphone->sample_buffer) {
+        RARCH_ERR("[CoreAudio]: Failed to create sample buffer\n");
+        goto error;
+    }
+
+    return microphone;
+
+error:
+    if (microphone) {
+        if (microphone->audio_unit)
+            AudioComponentInstanceDispose(microphone->audio_unit);
+        free(microphone);
+    }
+    return NULL;
+}
+
+/// Close microphone
+static void coreaudio_microphone_close_mic(void *driver_context, void *microphone_context)
+{
+    coreaudio_microphone_handle_t *microphone = (coreaudio_microphone_handle_t*)microphone_context;
+    if (microphone) {
+        if (microphone->is_running)
+            AudioOutputUnitStop(microphone->audio_unit);
+        AudioComponentInstanceDispose(microphone->audio_unit);
+        if (microphone->sample_buffer)
+            fifo_free(microphone->sample_buffer);
+        free(microphone);
+    } else {
+        RARCH_ERR("[CoreAudio]: Failed to close microphone\n");
+    }
+}
+
+/// Start microphone
+static bool coreaudio_microphone_start_mic(void *driver_context, void *microphone_context)
+{
+    coreaudio_microphone_handle_t *microphone = (coreaudio_microphone_handle_t*)microphone_context;
+    if (!microphone) {
+        RARCH_ERR("[CoreAudio]: Failed to start microphone\n");
+        return false;
+    }
+
+    OSStatus status = AudioOutputUnitStart(microphone->audio_unit);
+    if (status == noErr) {
+        microphone->is_running = true;
+        return true;
+    } else {
+        RARCH_ERR("[CoreAudio]: Failed to start microphone: %d\n", status);
+    }
+    return false;
+}
+
+/// Stop microphone
+static bool coreaudio_microphone_stop_mic(void *driver_context, void *microphone_context)
+{
+    coreaudio_microphone_handle_t *microphone = (coreaudio_microphone_handle_t*)microphone_context;
+    if (!microphone) {
+        RARCH_ERR("[CoreAudio]: Failed to stop microphone\n");
+        return false;
+    }
+
+    OSStatus status = AudioOutputUnitStop(microphone->audio_unit);
+    if (status == noErr) {
+        microphone->is_running = false;
+        return true;
+    } else {
+        RARCH_ERR("[CoreAudio]: Failed to stop microphone: %d\n", status);
+    }
+    return false;
+}
+
+/// Check if microphone is alive
+static bool coreaudio_microphone_mic_alive(const void *driver_context, const void *microphone_context)
+{
+    coreaudio_microphone_handle_t *microphone = (coreaudio_microphone_handle_t*)microphone_context;
+    (void)driver_context;
+
+    return microphone && microphone->is_running;
+}
+
+/// Check if microphone uses float samples
+static bool coreaudio_microphone_mic_use_float(const void *driver_context, const void *microphone_context)
+{
+    coreaudio_microphone_handle_t *microphone = (coreaudio_microphone_handle_t*)microphone_context;
+    (void)driver_context;
+
+    return microphone && microphone->use_float;
+}
+
+/// Get device list (not implemented for CoreAudio)
+static struct string_list *coreaudio_microphone_device_list_new(const void *driver_context)
+{
+    (void)driver_context;
+    return NULL;
+}
+
+/// Free device list (not implemented for CoreAudio)
+static void coreaudio_microphone_device_list_free(const void *driver_context, struct string_list *devices)
+{
+    (void)driver_context;
+    (void)devices;
+}
+
+/// CoreAudio microphone driver structure
+microphone_driver_t microphone_coreaudio = {
+    coreaudio_microphone_init,
+    coreaudio_microphone_free,
+    coreaudio_microphone_read,
+    coreaudio_microphone_set_nonblock_state,
+    "coreaudio",
+    coreaudio_microphone_device_list_new,
+    coreaudio_microphone_device_list_free,
+    coreaudio_microphone_open_mic,
+    coreaudio_microphone_close_mic,
+    coreaudio_microphone_mic_alive,
+    coreaudio_microphone_start_mic,
+    coreaudio_microphone_stop_mic,
+    coreaudio_microphone_mic_use_float
+};
