f98cc1dd03f3b55378f94e955af25799ad37219a
pabloarista <3964831+pabloarista@users.noreply.github.com>
2025-03-01 15:40:33 -0500

    fixed atari core mfi controllers that do NOT have l3/r3 buttons; added confirmation of deleting roms; added extensions to simplify localization; added fix for deleting multi disc/track files that don't always get deleted

--- PVUI/Sources/PVUIBase/UIViewController+Alerts.swift
+++ PVUI/Sources/PVUIBase/UIViewController+Alerts.swift
@@ -13,16 +13,49 @@ import PVLogging
 
 public
 extension UIViewController {
-    func presentMessage(_ message: String, title: String, source: UIView, completion _: (() -> Swift.Void)? = nil) {
-        NSLog("Title: %@ Message: %@", title, message);
+    func presentMessage(_ message: String, title: String, source: UIView, completion: (() -> Void)? = nil) {
+        presentMessage(message,
+                       title: title,
+                       source: source,
+                       completion: completion)
+    }
+    
+    func presentDeleteMessage(_ message: String, title: String, source: UIView, completion: (() -> Void)? = nil) {
+        presentMessage(message, title: title,
+                       source: source,
+                       secondaryActionTitle: Bundle.module.localized("Cancel"),
+                       secondaryActionStyle: .cancel,
+                       secondaryCompletion: nil,
+                       defaultActionTitle: Bundle.module.localized("Delete"),
+                       defaultActionStyle: .destructive,
+                       completion: completion)
+    }
+    
+    func presentMessage(_ message: String,
+                        title: String,
+                        source: UIView,
+                        secondaryActionTitle: String? = nil,
+                        secondaryActionStyle: UIAlertAction.Style = .cancel,
+                        secondaryCompletion: (() -> Void)? = nil,
+                        defaultActionTitle: String? = nil,
+                        defaultActionStyle: UIAlertAction.Style = .default,
+                        completion: (() -> Void)? = nil) {
+        DLOG("Title: \(title) Message: \(message) secondaryActionTitle: \(secondaryActionTitle) defaultActionTitle: \(defaultActionTitle)")
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.preferredContentSize = CGSize(width: 300, height: 300)
         
         alert.popoverPresentationController?.barButtonItem = self.navigationItem.leftBarButtonItem
         alert.popoverPresentationController?.sourceView = source
         alert.popoverPresentationController?.sourceRect = UIScreen.main.bounds
-        
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        let actualDefaultActionTitle = defaultActionTitle ?? Bundle.module.localized("OK")
+        alert.addAction(UIAlertAction(title: actualDefaultActionTitle, style: defaultActionStyle) { _ in
+            completion?()
+        })
+        if let actualSecondaryActionTitle = secondaryActionTitle {
+            alert.addAction(UIAlertAction(title: actualSecondaryActionTitle, style: secondaryActionStyle) { _ in
+                secondaryCompletion?()
+            })
+        }
         
         let presentingVC = presentedViewController ?? self
         
@@ -35,13 +68,23 @@ extension UIViewController {
         }
     }
 
-    func presentError(_ message: String, source: UIView, completion: (() -> Swift.Void)? = nil) {
+    func presentError(_ message: String, source: UIView, completion: (() -> Void)? = nil) {
         ELOG("\(message)")
-        presentMessage(message, title: "Error", source: source, completion: completion)
+        presentMessage(message, title: Bundle.module.localized("Error"), source: source, completion: completion)
     }
 
-    func presentWarning(_ message: String, source: UIView, completion: (() -> Swift.Void)? = nil) {
+    func presentWarning(_ message: String, source: UIView, completion: (() -> Void)? = nil) {
         WLOG("\(message)")
-        presentMessage(message, title: "Warning", source: source, completion: completion)
+        presentMessage(message, title: Bundle.module.localized("Warning"), source: source, completion: completion)
+    }
+}
+
+public extension Bundle {
+    func localized(_ key: String) -> String {
+        NSLocalizedString(key, bundle: self, comment: "")
+    }
+    
+    func localized(_ key: String,  _ arguments: any CVarArg...) -> String {
+        String(format: NSLocalizedString(key, bundle: self, comment: ""), arguments)
     }
 }
