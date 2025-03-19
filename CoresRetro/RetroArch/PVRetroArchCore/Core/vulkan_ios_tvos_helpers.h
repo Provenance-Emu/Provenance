5aa269cafa3fcf75d5a8ca137295588da94578df
Joseph Mattiello <git@joemattiello.com>
2025-03-19 00:07:16 -0400

    ra vulkan ios helpers
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- CoresRetro/RetroArch/PVRetroArchCore/Core/vulkan_ios_tvos_helpers.h
+++ CoresRetro/RetroArch/PVRetroArchCore/Core/vulkan_ios_tvos_helpers.h
@@ -7,6 +7,9 @@
 
 #include <vulkan/vulkan.h>
 
+/* Forward declarations */
+struct vk_buffer;
+
 #if defined(HAVE_COCOATOUCH) || defined(TARGET_OS_TV)
 /* Helper function to improve command buffer synchronization for iOS/tvOS */
 void vulkan_ios_tvos_command_buffer_sync(VkCommandBuffer cmd, VkQueue queue, VkDevice device);
@@ -23,6 +26,11 @@ void vulkan_ios_tvos_texture_barrier(VkCommandBuffer cmd,
  * Metal does not support disabling primitive restart, so this function sets
  * primitiveRestartEnable to VK_TRUE to avoid warnings */
 void vulkan_ios_tvos_setup_input_assembly(VkPipelineInputAssemblyStateCreateInfo *input_assembly);
+
+/* Helper function to update a buffer when memory mapping fails on iOS/tvOS
+ * This uses a staging buffer to update the contents of a buffer that cannot be mapped */
+bool vulkan_ios_tvos_update_buffer_without_mapping(VkDevice device, VkCommandBuffer cmd, 
+      struct vk_buffer *buffer, const void *data, size_t size);
 #endif
 
 #endif /* __VULKAN_IOS_TVOS_HELPERS_H */
