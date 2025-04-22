13e3c521e2466d9815112d9f0da826dbcb6c2c23
Joseph Mattiello <git@joemattiello.com>
2025-04-22 16:41:09 -0400

    game importer don’t reimport existing bioses
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- PVLibrary/Sources/PVLibrary/Importer/Services/GameImporter/GameImporter+Utils.swift
+++ PVLibrary/Sources/PVLibrary/Importer/Services/GameImporter/GameImporter+Utils.swift
@@ -41,7 +41,34 @@ extension GameImporter {
     }
 
     internal func isBIOS(_ queueItem: ImportQueueItem) -> Bool {
-        // First check by filename
+        // First check if the file is already in a BIOS subdirectory
+        let path = queueItem.url.path
+        if path.contains("/BIOS/") {
+            // If it's already in a BIOS directory, check if we already have a PVBIOS entry with a file for it
+            let filename = queueItem.url.lastPathComponent.lowercased()
+            
+            // Check if any BIOS entry already has this file
+            let biosEntries = PVEmulatorConfiguration.biosEntries
+            let matchingEntries = biosEntries.filter { bios in
+                // Check if the BIOS entry has a file and the filename matches
+                if let file = bios.file {
+                    return file.fileName.lowercased() == filename
+                }
+                return false
+            }
+            
+            // If we found matching entries with files, don't add to import queue
+            if !matchingEntries.isEmpty {
+                ILOG("BIOS file \(filename) already has a PVBIOS entry with a file, skipping import")
+                return false
+            }
+            
+            // If we're in a BIOS directory but don't have a matching entry with a file, we should import it
+            ILOG("BIOS file \(filename) is in BIOS directory but needs to be imported")
+            return true
+        }
+        
+        // Check by filename against our known BIOS filenames
         let filename = queueItem.url.lastPathComponent.lowercased()
         if RomDatabase.biosFilenamesCache.contains(filename) {
             DLOG("Found BIOS match by filename: \(filename)")
