799f108dcc4c3581a8b92e8b507e8c6ffd63a0d3
pabloarista <3964831+pabloarista@users.noreply.github.com>
2025-04-22 22:43:42 -0400

    updated to new protocol changes

--- PVLibrary/Sources/PVLibrary/Importer/iCloud/SyncErrorHandler.swift
+++ PVLibrary/Sources/PVLibrary/Importer/iCloud/SyncErrorHandler.swift
@@ -26,27 +26,30 @@ public struct iCloudSyncError {
 /// Protocol for handling sync errors
 public protocol SyncErrorHandler {
     /// Get all error summaries
-    var allErrorSummaries: [String] { get throws }
+    var allErrorSummaries: [String] { get async throws }
     
     /// Get all full errors
-    var allFullErrors: [String] { get throws }
+    var allFullErrors: [String] { get async throws }
     
     /// Get all errors
-    var allErrors: [iCloudSyncError] { get }
+    var allErrors: [iCloudSyncError] { get async }
+
+    /// true if number of errors is 0 and false otherwise
+    var isEmpty: Bool { get async }
     
     /// Number of errors
-    var numberOfErrors: Int { get }
+    var numberOfErrors: Int { get async }
     
     /// Handle an error with a file
     /// - Parameters:
     ///   - error: The error to handle
     ///   - file: The file associated with the error
-    func handleError(_ error: Error, file: URL?)
+    func handleError(_ error: Error, file: URL?) async
     
     /// Clear all errors
-    func clear()
+    func clear() async
     
     /// Handle an error with optional context
     /// - Parameter error: The error to handle
-    func handle(error: Error)
+    func handle(error: Error) async
 }
