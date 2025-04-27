6f8ef7b59a63e337c3b13a695b0a36f4b825f99b
Joseph Mattiello <git@joemattiello.com>
2025-04-27 04:01:51 -0400

    add notifications from pvwebserver
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- PVUI/Sources/PVUIBase/NSNotification+Library.swift
+++ PVUI/Sources/PVUIBase/NSNotification+Library.swift
@@ -25,4 +25,27 @@ public extension Notification.Name {
     static let PVRefreshLibrary = Notification.Name("kRefreshLibraryNotification")
     static let PVRefreshLibraryFinished = Notification.Name("kRefreshLibraryFinishedNotification") // Added for refresh completion
     static let PVInterfaceDidChangeNotification = Notification.Name("kInterfaceDidChangeNotification")
+    
+    // Progress notification names
+    static let cacheManagementProgress = Notification.Name("CacheManagementProgress")
+    static let downloadProgress = Notification.Name("DownloadProgress")
+    static let romScanningProgress = Notification.Name("ROMScanningProgress")
+    static let romScanningCompleted = Notification.Name("ROMScanningCompleted")
+    
+    // Web server upload notifications
+    static let webServerUploadProgress = Notification.Name("WebServerUploadProgress")
+    static let webServerUploadCompleted = Notification.Name("WebServerUploadCompleted")
+    static let webServerStatusChanged = Notification.Name("WebServerStatusChanged")
+    
+    // PVWebServer file upload notifications
+    static let webServerFileUploadStarted = Notification.Name("PVWebServerFileUploadStartedNotification")
+    static let webServerFileUploadProgress = Notification.Name("PVWebServerFileUploadProgressNotification")
+    static let webServerFileUploadCompleted = Notification.Name("PVWebServerFileUploadCompletedNotification")
+    static let webServerFileUploadFailed = Notification.Name("PVWebServerFileUploadFailedNotification")
+    
+    // Archive extraction notifications
+    static let archiveExtractionStarted = Notification.Name("ArchiveExtractionStarted")
+    static let archiveExtractionProgress = Notification.Name("ArchiveExtractionProgress")
+    static let archiveExtractionCompleted = Notification.Name("ArchiveExtractionCompleted")
+    static let archiveExtractionFailed = Notification.Name("ArchiveExtractionFailed")
 }
