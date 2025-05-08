1c4fbd4a3413093b5c07940af801949f0abee988
Joseph Mattiello <git@joemattiello.com>
2025-05-07 21:00:09 -0400

    retroarch fix saturn controls
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- CoresRetro/RetroArch/PVRetroArchCore/Swift/PVRetroArchCoreCore.swift
+++ CoresRetro/RetroArch/PVRetroArchCore/Swift/PVRetroArchCoreCore.swift
@@ -423,3 +423,18 @@ extension PVRetroArchCoreCore: PVPSPSystemResponderClient {
         (_bridge as! PVPSPSystemResponderClient).didPush(button, forPlayer: player)
     }
 }
+
+// Sega Saturn
+extension PVRetroArchCoreCore: PVSaturnSystemResponderClient {
+    public func didRelease(_ button: PVCoreBridge.PVSaturnButton, forPlayer player: Int) {
+        (_bridge as! PVSaturnSystemResponderClient).didRelease(button, forPlayer: player)
+
+    }
+    public func didMoveJoystick(_ button: PVCoreBridge.PVSaturnButton, withXValue xValue: CGFloat, withYValue yValue: CGFloat, forPlayer player: Int) {
+        (_bridge as! PVSaturnSystemResponderClient).didMoveJoystick(button, withXValue: xValue, withYValue: yValue, forPlayer: player)
+
+    }
+    public func didPush(_ button: PVCoreBridge.PVSaturnButton, forPlayer player: Int) {
+        (_bridge as! PVSaturnSystemResponderClient).didPush(button, forPlayer: player)
+    }
+}
