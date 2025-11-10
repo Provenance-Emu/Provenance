29b1990a67b27468504cf9f9e79821c9e9fc989a
Joseph Mattiello <git@joemattiello.com>
2025-11-10 15:46:37 -0500

    bump version to 3.2.0
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Provenance/Main UI/ProvenanceApp.swift
+++ Provenance/Main UI/ProvenanceApp.swift
@@ -923,7 +923,7 @@ extension ProvenanceApp: WhatsNewCollectionProvider {
             )
         )
         WhatsNew(
-            version: "3.1.2",
+            version: "3.2.0",
             title: "3DS Keyboard Fix & Controller Remapping, Universal Skin Support, and Stability Improvements, MAME ZIP Support",
             features: [
                 .init(
