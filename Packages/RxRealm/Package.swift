2b49a4dbb8e26b90c045e8b856e95c65874fc24a
Joseph Mattiello <git@joemattiello.com>
2026-03-04 23:27:45 -0500

    build: bump swift-tools-version to 5.9, platforms to iOS/tvOS 17

--- Packages/RxRealm/Package.swift
+++ Packages/RxRealm/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(name: "RxRealm",
                       platforms: [
-                        .macOS(.v10_10), .iOS(.v17), .tvOS(.v17), .watchOS(.v3)
+                        .macOS(.v14), .iOS(.v17), .tvOS(.v17), .watchOS(.v9)
                       ],
                       products: [
                         // Products define the executables and libraries produced by a package, and make them visible to other packages.
