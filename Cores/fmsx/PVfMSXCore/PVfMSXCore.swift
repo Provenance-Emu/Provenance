ffc4ab322754c3e1aabee13fa0e8e08a9f83baa7
Joseph Mattiello <git@joemattiello.com>
2026-03-11 16:09:13 -0400

    add fMSX to build PVCore
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Cores/fmsx/PVfMSXCore/PVfMSXCore.swift
+++ Cores/fmsx/PVfMSXCore/PVfMSXCore.swift
@@ -32,52 +32,52 @@ open class PVfMSXEmuCore: PVEmulatorCore {
 
 extension PVfMSXEmuCore: PVMSXSystemResponderClient {
     public func didPush(_ button: PVCoreBridge.PVMSXButton, forPlayer player: Int) {
-        _bridge.didPush(button, forPlayer: player)
+        (_bridge as? PVMSXSystemResponderClient)?.didPush(button, forPlayer: player)
     }
     public func didRelease(_ button: PVCoreBridge.PVMSXButton, forPlayer player: Int) {
-        _bridge.didRelease(button, forPlayer: player)
+        (_bridge as? PVMSXSystemResponderClient)?.didRelease(button, forPlayer: player)
     }
 
-    public var gameSupportsKeyboard: Bool { _bridge.gameSupportsKeyboard }
-    public var requiresKeyboard: Bool { _bridge.requiresKeyboard }
+    public var gameSupportsKeyboard: Bool { (_bridge as? PVMSXSystemResponderClient)?.gameSupportsKeyboard ?? false }
+    public var requiresKeyboard: Bool { (_bridge as? PVMSXSystemResponderClient)?.requiresKeyboard ?? false }
 #if canImport(GameController)
     public func keyDown(_ key: GCKeyCode) {
-        _bridge.keyDown(key)
+        (_bridge as? PVMSXSystemResponderClient)?.keyDown(key)
     }
     public func keyUp(_ key: GCKeyCode) {
-        _bridge.keyUp(key)
+        (_bridge as? PVMSXSystemResponderClient)?.keyUp(key)
     }
 #endif
 
-    public var gameSupportsMouse: Bool { _bridge.gameSupportsMouse }
-    public var requiresMouse: Bool { _bridge.requiresMouse }
+    public var gameSupportsMouse: Bool { (_bridge as? PVMSXSystemResponderClient)?.gameSupportsMouse ?? false }
+    public var requiresMouse: Bool { (_bridge as? PVMSXSystemResponderClient)?.requiresMouse ?? false }
 
 #if canImport(GameController)
     public func didScroll(_ cursor: GCDeviceCursor) {
-        _bridge.didScroll(cursor)
+        (_bridge as? PVMSXSystemResponderClient)?.didScroll(cursor)
     }
-    public var mouseMovedHandler: GCMouseMoved? { _bridge.mouseMovedHandler }
+    public var mouseMovedHandler: GCMouseMoved? { (_bridge as? PVMSXSystemResponderClient)?.mouseMovedHandler }
 #endif
 
     public func mouseMoved(atPoint point: CGPoint) {
-        _bridge.mouseMovedAtPoint(point)
+        (_bridge as? PVMSXSystemResponderClient)?.mouseMoved(at: point)
     }
     public func mouseMoved(at point: CGPoint) {
-        _bridge.mouseMovedAt(point)
+        (_bridge as? PVMSXSystemResponderClient)?.mouseMoved(at: point)
     }
     public func leftMouseDown(at point: CGPoint) {
-        _bridge.leftMouseDown(at: point)
+        (_bridge as? PVMSXSystemResponderClient)?.leftMouseDown(at: point)
     }
     public func leftMouseDown(atPoint point: CGPoint) {
-        _bridge.leftMouseDown(atPoint: point)
+        (_bridge as? PVMSXSystemResponderClient)?.leftMouseDown(atPoint: point)
     }
     public func leftMouseUp() {
-        _bridge.leftMouseUp()
+        (_bridge as? PVMSXSystemResponderClient)?.leftMouseUp()
     }
     public func rightMouseDown(atPoint point: CGPoint) {
-        _bridge.rightMouseDown(atPoint: point)
+        (_bridge as? PVMSXSystemResponderClient)?.rightMouseDown(atPoint: point)
     }
     public func rightMouseUp() {
-        _bridge.rightMouseUp()
+        (_bridge as? PVMSXSystemResponderClient)?.rightMouseUp()
     }
 }
