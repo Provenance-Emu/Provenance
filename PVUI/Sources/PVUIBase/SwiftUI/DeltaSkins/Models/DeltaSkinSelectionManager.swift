59f8fdcfdea2b28fb9b9546c4f8688cd76b57a5a
Joseph Mattiello <git@joemattiello.com>
2026-04-01 19:02:21 -0400

    fix FT call
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- PVUI/Sources/PVUIBase/SwiftUI/DeltaSkins/Models/DeltaSkinSelectionManager.swift
+++ PVUI/Sources/PVUIBase/SwiftUI/DeltaSkins/Models/DeltaSkinSelectionManager.swift
@@ -315,7 +315,7 @@ public final class DeltaSkinSelectionManager: ObservableObject {
         ILOG("skins: Effective skin '\(skin.name)' doesn't support \(orientation.rawValue), finding fallback")
 
         // Try to find first available skin that supports this orientation
-        let allowCaseSkins = PVFeatureFlagsManager.shared.caseCompanionSkins && CaseControllerDetector.isKnownPhysicalCaseControllerConnected
+        let allowCaseSkins = PVFeatureFlags.shared.isEnabled(.caseCompanionSkins) && CaseControllerDetector.isKnownPhysicalCaseControllerConnected
         for fallbackSkin in availableSkins {
             // Skip the current skin
             if fallbackSkin.identifier == skinId {
